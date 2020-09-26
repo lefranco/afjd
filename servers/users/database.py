654b5c9f6b71ac0305a7eb18680e4f498c9f1359
lefranco <jeremie.lefrancois@gmail.com>
2020-09-26 21:30:04 +0200

    .

---
+++ servers/users/database.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+
+"""
+File : database.py
+
+Interface with the sqlite database
+"""
+import sqlite3
+import typing
+import pathlib
+import unicodedata
+
+# File holding the SQLITE database
+FILE = "./db/users.db"
+
+BYTES_SEPARATOR = b';'
+STR_SEPARATOR = ';'
+STR_SEPARATOR_SUBSTITUTE = ':'
+
+
+def sanitize_field(field_content: str) -> str:
+    """ Any str to something that goes into the database """
+
+    assert isinstance(field_content, str), "Sanitize applicable only to strings"
+    nfkd_form = unicodedata.normalize('NFKD', field_content)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    back_str = only_ascii.decode()
+    without_separator = back_str.replace(STR_SEPARATOR, STR_SEPARATOR_SUBSTITUTE)
+    return without_separator
+
+
+def db_remove() -> None:
+    """ For testing puprpose"""
+
+    db_file = pathlib.Path(FILE)
+    if not db_file.is_file():
+        return
+    db_file.unlink()
+
+
+def db_present() -> bool:
+    """ Answers the question"""
+
+    db_file = pathlib.Path(FILE)
+    return db_file.is_file()
+
+
+def sql_execute(command: str, parameters: typing.Optional[typing.Tuple[typing.Any, ...]] = None, need_result: bool = False) -> typing.Optional[typing.List[typing.Any]]:
+    """ Executes a sql command """
+
+    # Note : ignoring Ctrl-C is not possible in flask-rest context
+
+    connection = sqlite3.connect(FILE, detect_types=sqlite3.PARSE_DECLTYPES)
+    cursor = connection.cursor()
+    if parameters:
+        cursor.execute(command, parameters)
+    else:
+        cursor.execute(command)
+    result = cursor.fetchall() if need_result else None
+    cursor.close()
+    connection.commit()  # necessary otherwise nothing happens
+    connection.close()
+
+    return result
+
+
+if __name__ == '__main__':
+    assert False, "Do not run this script"
