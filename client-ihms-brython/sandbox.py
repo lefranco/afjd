1fe2047605e78b6103fd6e5beb39ff0718b8e6d3
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-02-11 19:25:26 +0100

    .

---
+++ client-ihms-brython/sandbox.py
@@ -0,0 +1,18 @@
+""" master """
+
+# pylint: disable=pointless-statement, expression-not-assigned
+
+from browser import html  # pylint: disable=import-error
+
+
+my_panel = html.DIV(id="sandbox")
+
+
+my_panel <= """
+  Here go widgets simulate orders in a fake game
+"""
+
+
+def render(panel_middle) -> None:
+    """ render """
+    panel_middle <= my_panel
