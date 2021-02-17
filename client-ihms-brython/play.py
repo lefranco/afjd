3205ab3814dd5b9ee5ba02e05906a581ebfe675a
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-02-17 07:50:01 +0100

    .

--- client-ihms-brython/play.py
+++ client-ihms-brython/play.py
@@ -2,9 +2,14 @@
 
 # pylint: disable=pointless-statement, expression-not-assigned
 
-from browser import html  # pylint: disable=import-error
+import json
 
-OPTIONS = ['submit orders', 'negotiate']
+from browser import html, ajax, alert   # pylint: disable=import-error
+from browser.local_storage import storage  # pylint: disable=import-error
+
+import config
+
+OPTIONS = ['submit orders', 'negotiate', 'display parameters']
 
 my_panel = html.DIV(id="play")
 my_panel.attrs['style'] = 'display: table-row'
@@ -39,6 +44,83 @@ def negotiate():
     my_sub_panel <= dummy
 
 
+def display_parameters():
+    """ display_parameters """
+
+    if 'GAME' not in storage:
+        alert("Please select game beforehand")
+        return
+
+    game = storage['GAME']
+    parameters_loaded = None
+
+    def display_all_parameters_reload():
+        """ change_description_reload """
+
+        status = True
+
+        def local_noreply_callback(_):
+            """ noreply_callback """
+            nonlocal status
+            alert("Problem (no answer from server)")
+            status = False
+
+        def reply_callback(req):
+            """ reply_callback """
+            nonlocal status
+            nonlocal parameters_loaded
+
+            req_result = json.loads(req.text)
+            if req.status != 200:
+                alert(f"Problem loading all parameters: {req_result['msg']}")
+                status = False
+                return
+
+            parameters_loaded = req_result
+
+        json_dict = dict()
+
+        host = config.SERVER_CONFIG['GAME']['HOST']
+        port = config.SERVER_CONFIG['GAME']['PORT']
+        url = f"{host}:{port}/games/{game}"
+
+        ajax.get(url, blocking=True, headers={'content-type': 'application/json'}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=local_noreply_callback)
+
+        return status
+
+    status = display_all_parameters_reload()
+    if not status:
+        return
+
+    game_params_table = html.TABLE()
+    game_params_table.style = {
+        "padding": "5px",
+        "backgroundColor": "#aaaaaa",
+        "border": "solid",
+    }
+    for key, value in parameters_loaded.items():
+        row = html.TR()
+        row.style = {
+            "border": "solid",
+        }
+
+        col1 = html.TD(key)
+        col1.style = {
+            "border": "solid",
+        }
+        row <= col1
+
+        col2 = html.TD(value)
+        col2.style = {
+            "border": "solid",
+        }
+        row <= col2
+
+        game_params_table <= row
+
+    my_sub_panel <= game_params_table
+
+
 def load_option(_, item_name):
     """ load_option """
 
@@ -47,6 +129,9 @@ def load_option(_, item_name):
         submit_orders()
     if item_name == 'negotiate':
         negotiate()
+    if item_name == 'display parameters':
+        display_parameters()
+
     global item_name_selected  # pylint: disable=invalid-name
     item_name_selected = item_name
 
