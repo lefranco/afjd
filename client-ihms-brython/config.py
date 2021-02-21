8f695a4e11795ca7190a488ffe1ddb3ff4bfff1a
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-02-21 19:28:11 +0100

    .

--- client-ihms-brython/config.py
+++ client-ihms-brython/config.py
@@ -1,31 +1,46 @@
 """ config """
 
-# config of servers
+import configparser
+import collections
 
-SERVER_CONFIG = dict()
-
-SERVER_CONFIG['USER'] = dict()
-SERVER_CONFIG['USER']['HOST'] = "https://afjd1.eu.ngrok.io"
-SERVER_CONFIG['USER']['PORT'] = 443
-
-SERVER_CONFIG['EMAIL'] = dict()
-SERVER_CONFIG['EMAIL']['HOST'] = "https://afjd2.eu.ngrok.io"
-SERVER_CONFIG['EMAIL']['PORT'] = 443
-
-SERVER_CONFIG['PLAYER'] = dict()
-SERVER_CONFIG['PLAYER']['HOST'] = "https://afjd3.eu.ngrok.io"
-SERVER_CONFIG['PLAYER']['PORT'] = 443
-
-SERVER_CONFIG['GAME'] = dict()
-SERVER_CONFIG['GAME']['HOST'] = "https://afjd4.eu.ngrok.io"
-SERVER_CONFIG['GAME']['PORT'] = 443
-
-SERVER_CONFIG['SOLVER'] = dict()
-SERVER_CONFIG['SOLVER']['HOST'] = "https://afjd5.eu.ngrok.io"
-SERVER_CONFIG['SOLVER']['PORT'] = 443
 
 # timeout for server requests
 TIMEOUT_SERVER = 2
 
 # timeout for message when OK
 REMOVE_AFTER = 5
+
+
+class ConfigFile:
+    """    Just reads an ini file   """
+
+    def __init__(self, filename: str):
+        self._config = configparser.ConfigParser(inline_comment_prefixes='#',    # do not accept ';'
+                                                 empty_lines_in_values=False,    # as it says
+                                                 interpolation=None)             # do not use variables
+
+        assert self._config.read(filename, encoding='utf-8'), f"Missing ini file named {filename}"
+
+    def section(self, section_name: str):
+        """ Accesses a section of a config file  """
+        assert self._config.has_section(section_name), "Missing section in ini file"
+        return self._config[section_name]
+
+    def section_list(self):
+        """ Accesses the list of sections of a config file  """
+        return self._config.sections()
+
+SERVER_CONFIG = collections.defaultdict(dict)
+
+def load_servers_config() -> None:
+    """ read servers config """
+
+    global SERVER_CONFIG
+
+    servers_config = ConfigFile('./config/servers.ini')
+    for server in servers_config.section_list():
+        server_data = servers_config.section(server)
+        SERVER_CONFIG[server]['HOST'] = server_data['HOST']
+        SERVER_CONFIG[server]['PORT'] = int(server_data['PORT'])
+
+load_servers_config()
