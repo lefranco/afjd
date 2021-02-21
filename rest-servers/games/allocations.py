f4dd6d862afabb1b3c2033d46c30fd307ab06ec8
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-02-21 18:42:04 +0100

    .

--- rest-servers/games/allocations.py
+++ rest-servers/games/allocations.py
@@ -36,7 +36,8 @@ class Allocation:
         allocations_found = database.sql_execute("SELECT * FROM allocations", need_result=True)
         if not allocations_found:
             return []
-        allocations_list = [a[0] for a in allocations_found]
+        print(f"{allocations_found=}")
+        allocations_list = [a for a in allocations_found]
         return allocations_list
 
     @staticmethod
