dd1f699176bb67e6f2650d57a3dd6cb89e2d6733
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-04 07:35:23 +0100

    .

--- client-ihms-brython/technical.py
+++ client-ihms-brython/technical.py
@@ -14,7 +14,7 @@ link1 = html.A(href="https://media.wizards.com/2015/rules/diplomacy_rules.pdf")
 link1 <= "Link to the official rules of the game"
 my_panel  <= link1
 
-title2 = html.H2("Adjudication algorithm")
+title2 = html.H2("Overall adjudication algorithm (D.A.T.C.)")
 my_panel  <= title2
 
 link2 = html.A(href="http://web.inter.nl.net/users/L.B.Kruijswijk/")
@@ -24,8 +24,10 @@ my_panel  <= link2
 title3 = html.H2("Implementation choices")
 my_panel  <= title3
 
-iframe = html.IFRAME(src="./docs/Compl_en.pdf", width='100%', height=500)
-my_panel <= iframe
+link3 = html.A(href="./docs/Compl_en.pdf")
+link3 <= "Link to the implementation choices made for the adjudication engine"
+my_panel  <= link3
+
 
 
 def render(panel_middle):
