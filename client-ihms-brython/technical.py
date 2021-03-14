c16eeba4ab953eab5d1eed2faf01873065108a6f
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-14 19:15:29 +0100

    .

--- client-ihms-brython/technical.py
+++ client-ihms-brython/technical.py
@@ -7,12 +7,16 @@ from browser import html  # pylint: disable=import-error
 
 my_panel = html.DIV(id="technical")
 
+# --
+
 title1 = html.H2("Rules of the game")
 my_panel <= title1
 
 link1 = html.A(href="https://media.wizards.com/2015/rules/diplomacy_rules.pdf")
 link1 <= "Link to the official rules of the game"
 my_panel <= link1
+
+# --
 
 title2 = html.H2("Overall adjudication algorithm (D.A.T.C.)")
 my_panel <= title2
@@ -21,12 +25,23 @@ link2 = html.A(href="http://web.inter.nl.net/users/L.B.Kruijswijk/")
 link2 <= "Link to the algorithm used for adjudication component"
 my_panel <= link2
 
+# --
+
 title3 = html.H2("Implementation choices")
 my_panel <= title3
 
 link3 = html.A(href="./docs/Compl_en.pdf")
 link3 <= "Link to the implementation choices made for the adjudication engine"
 my_panel <= link3
+
+# --
+
+title4 = html.H2("Special thanks")
+my_panel <= title4
+
+link4 = html.A(href="https://brython.info/")
+link4 <= "Tool used for this front end"
+my_panel <= link4
 
 
 def render(panel_middle):
