7c4ef8549ee43f5a8ec1cb17296a59b621d57d0b
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-14 19:15:29 +0100

    .

--- client-ihms-brython/home.py
+++ client-ihms-brython/home.py
@@ -7,14 +7,19 @@ from browser import html  # pylint: disable=import-error
 
 my_panel = html.DIV(id="home")
 
-my_panel <= """
-Below rules of the games (simplified)
-"""
+title1 = html.H2("Useful link")
+my_panel <= title1
+
+link1 = html.A(href="http://www.diplomania.fr")
+link1 <= "Le site officiel pour jouer de l'Association Française des Joueurs de Diplomatie"
+my_panel <= link1
+
+my_panel <= html.H2("Below rules of the games (simplified)")
 
 my_panel <= html.BR()
 my_panel <= html.BR()
 
-iframe = html.IFRAME(src="./docs/Summary_rules_en.html", width='100%', height=500)
+iframe = html.IFRAME(src="./docs/Summary_rules_fr.html", width='100%', height=500)
 my_panel <= iframe
 
 
