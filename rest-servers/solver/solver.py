c33fee07a83514fda33bb29b880c243453d6a3db
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-15 19:03:57 +0100

    .

--- rest-servers/solver/solver.py
+++ rest-servers/solver/solver.py
@@ -334,7 +334,7 @@ def read_situation(situation_result_content: typing.List[str], variant: typing.D
     region_names = [r.upper() for r in names['zones'].values() if r]
     zone_names = [r.upper() for r in names['zones'].values() if r]
     zone_names += [f"{names['zones'][str(r)]}{names['coasts'][str(c)]}".upper() for r, c in variant['coastal_zones']]
-    role_names = [d[0].upper() for d in names['roles'].values()]
+    role_names = [d[0].upper() for d in names['roles'].values() if d]
     center_table = variant['centers']
     type_names = ["A", "F"]
 
