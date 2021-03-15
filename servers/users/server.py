efea38b536fae18be78dddb8e2deb7a462d88be2
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-15 23:22:07 +0100

    .

--- servers/users/server.py
+++ servers/users/server.py
@@ -32,11 +32,12 @@ SECRET_CONFIG = lowdata.ConfigFile('./config/secret.ini')
 SECRET_DATA = SECRET_CONFIG.section('JWT_SECRET_KEY')
 APP.config['JWT_SECRET_KEY'] = SECRET_DATA['key']
 
+# default is 15 minutes - put it to one hour !
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=60)
+
 # Seems JWT variable is not used in this implementation but could be later on...
 JWT = flask_jwt_extended.JWTManager(APP)
 
-# default is 15 minutes - put it to one hour
-JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
 
 # ---------------------------------
 # users
