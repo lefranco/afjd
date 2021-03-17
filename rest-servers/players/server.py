ecfcb5b799e3a9fcb590a0ae380cf270db9b8af7
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-17 10:19:36 +0100

    .

--- rest-servers/players/server.py
+++ rest-servers/players/server.py
@@ -401,6 +401,7 @@ class NewsRessource(flask_restful.Resource):  # type: ignore
         mylogger.LOGGER.info("/news - GET - get the latest news")
 
         newss_list = newss.News.inventory()
+        print(f"{newss_list=}")
         data = newss_list[0]
 
         return data, 200
