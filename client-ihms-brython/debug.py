a787abaff9a31ea52931e24b954fb7f7eaac9c5e
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-03-21 11:15:06 +0100

    .

--- client-ihms-brython/debug.py
+++ client-ihms-brython/debug.py
@@ -6,16 +6,19 @@
 import sys
 
 from browser.widgets.dialog import InfoDialog  # pylint: disable=import-error
-from browser import document, alert, html
+from browser import alert  # pylint: disable=import-error
 
 class Out:
-    def write(self, out):
+    """ Out """
+    def write(self, out):  # pylint: disable=no-self-use
+        """ write """
         InfoDialog("DEBUG", out)
 
 class Err:
-    def write(self, err):
+    """ Err """
+    def write(self, err):  # pylint: disable=no-self-use
+        """ write """
         alert(err)
 
 sys.stdout = Out()
 sys.stderr = Err()
-
