5c2ceef319083e6231f60dc4c1d32370cfdd3d95
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-04-03 12:14:09 +0200

    .

--- rest-servers/games/actives.py
+++ rest-servers/games/actives.py
@@ -39,8 +39,7 @@ class Active:
 
     def update_database(self) -> None:
         """ Pushes changes from object to database """
-        database.sql_execute("DELETE FROM actives WHERE game_id = ? AND role_num = ?", (self._game_id, self._role_num))
-        database.sql_execute("INSERT OR REPLACE INTO actives (game_id, role_num, active) VALUES (?, ?, ?)", (self._game_id, self._role_num, self._active))
+        database.sql_execute("INSERT OR REPLACE INTO actives (game_id, role_num) VALUES (?, ?)", (self._game_id, self._role_num))
 
     def delete_database(self) -> None:
         """ Removes object from database """
