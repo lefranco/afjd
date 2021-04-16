fc36fd285cd38e972a4ef37b1007887ef1df8b15
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-04-16 19:11:18 +0200

    .

--- rest-servers/games/submissions.py
+++ rest-servers/games/submissions.py
@@ -39,6 +39,7 @@ class Submission:
 
     def update_database(self) -> None:
         """ Pushes changes from object to database """
+        database.sql_execute("DELETE FROM submissions WHERE game_id = ? AND role_num = ?", (self._game_id, self._role_num))
         database.sql_execute("INSERT OR REPLACE INTO submissions (game_id, role_num) VALUES (?, ?)", (self._game_id, self._role_num))
 
     def delete_database(self) -> None:
