9ac6ec597014b8d0546726718c9b7d76624aa3e0
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-05-02 17:15:16 +0200

    .

--- client-ihms-brython/center_design.py
+++ client-ihms-brython/center_design.py
@@ -1,6 +1,9 @@
 """ center_design """
 
+# pylint: disable=multiple-statements
+
 import math
+
 
 def stabbeur_center(x: int, y: int, ctx):  # pylint: disable=invalid-name
     """ display a center the stabbeur way """
