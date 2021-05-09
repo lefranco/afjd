adba677086577365a4ccf83e0310b24e3476aed2
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-05-09 18:36:18 +0200

    .

--- servers/users/curl.sh
+++ servers/users/curl.sh
@@ -11,12 +11,12 @@
 # Register a login (exports a token)  - success good password
 #curl -H "Content-Type: application/json" -X POST   -d '{"user_name":"John","password":"Doe"}' https://afjd1.eu.ngrok.io:443/login
 
-# you must do export ACCESS= < result previous comand > to be granted access
+# you must do export ACCESS= < result previous command > to be granted access
 
 # Change password account (need token)
 #curl -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS" -X POST -d '{"user_name":"John","password":"Didi"}' https://afjd1.eu.ngrok.io:443/change
 
-# you must do export ACCESS= < result previous comand > to be granted access
+# you must do export ACCESS= < result previous command > to be granted access
 
 # Checks token (should succeed)
 #curl -H "Authorization: Bearer $ACCESS" https://afjd1.eu.ngrok.io:443/verify
