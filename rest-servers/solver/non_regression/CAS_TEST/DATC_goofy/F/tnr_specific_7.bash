e7b3ce3ad09637b5cb4afe52a2cd01f29053b3b5
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-10-08 20:42:34 +0200

    .

--- rest-servers/solver/non_regression/CAS_TEST/DATC_goofy/F/tnr_specific_7.bash
+++ rest-servers/solver/non_regression/CAS_TEST/DATC_goofy/F/tnr_specific_7.bash
@@ -19,22 +19,23 @@ fi
 sed 's/;.*$//' $pref.TXT > $pref.TXT2
 
 rm -f $pref.ORD
-if ! traducteur $pref.TXT $pref.ORD "${DIPLOCOM}/traduction/lexique_$lexique.txt" "${DIPLOCOM}/traduction/filtre.txt"  ; then
+if ! traducteur $pref.TXT2 $pref.ORD "${DIPLOCOM}/traduction/lexique_$lexique.txt" "${DIPLOCOM}/traduction/filtre.txt"  ; then
 	echo Probleme traducteur
     exit 255
 fi
 rm $pref.TXT2
 
 rm -f $pref.DAT
+aideur -c $carte -s -S A01  -o $pref.ORD -i $pref.DAT 
 if ! aideur -c $carte -s -S A01  -o $pref.ORD -i $pref.DAT 2>/dev/null ; then
 	echo Probleme aideur
     exit 255
 fi
 
-rm -f $pref.ORD
 rm -f $pref.TM1
 rm -f $pref.TM2
 
+ solveur -en -c $carte -s -o $pref.ORD -i $pref.DAT -a $pref.TM1 -f $pref.TM3  
 if solveur -en -c $carte -s -o $pref.ORD -i $pref.DAT -a $pref.TM1 -f $pref.TM3  2>$pref.TM2 ; then
     if diff $pref.TM3 $pref.RES 1>/dev/null 2>/dev/null ; then
 		  retour=0
@@ -44,5 +45,9 @@ if solveur -en -c $carte -s -o $pref.ORD -i $pref.DAT -a $pref.TM1 -f $pref.TM3 
 else
 	retour=1
 fi
+
+rm -f $pref.ORD
+rm -f $pref.TM1
+rm -f $pref.TM2
 
 exit $retour
