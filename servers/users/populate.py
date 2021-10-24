bd4d87d9f0206f4cb945fcde0050186d7f24b63d
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-10-24 19:33:53 +0200

    .

--- servers/users/populate.py
+++ servers/users/populate.py
@@ -9,6 +9,7 @@ Data : populate database
 
 import mylogger
 import users
+import logins
 import database
 
 
@@ -17,12 +18,17 @@ def populate_users(sql_executor: database.SqlExecutor) -> None:
 
     users.User.create_table(sql_executor)
 
+def populate_logins(sql_executor: database.SqlExecutor) -> None:
+    """ inserts these items in database """
+
+    logins.Login.create_table(sql_executor)
 
 def populate(sql_executor: database.SqlExecutor) -> None:
     """ inserts all items in database """
 
     mylogger.LOGGER.warning("Populating...")
     populate_users(sql_executor)
+    populate_logins(sql_executor)
 
 
 if __name__ == '__main__':
