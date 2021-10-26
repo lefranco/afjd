6a2fa3668d692150d4cfbc70859a08181c70caf9
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-10-26 08:42:48 +0200

    .

--- client-ihms-brython/oracle.py
+++ client-ihms-brython/oracle.py
@@ -155,6 +155,9 @@ def oracle():
     buttons_right = html.DIV(id='buttons_right')
     buttons_right.attrs['style'] = 'display: table-cell; width: 15%; vertical-align: top;'
 
+    legend_change_position = html.LEGEND(f"Utilisez le bac à sable pour changer la position")
+    buttons_right <= legend_change_position
+    buttons_right <= html.BR()
     put_consult(buttons_right)
 
     # overall
