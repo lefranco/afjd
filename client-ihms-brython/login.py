741d0097b46b443b72150234fc4d5d02ed0bdecf
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2021-11-09 15:05:22 +0100

    .

--- client-ihms-brython/login.py
+++ client-ihms-brython/login.py
@@ -14,6 +14,146 @@ import config
 import common
 
 
+my_panel = html.DIV(id="login")  # pylint: disable=invalid-name
+
+
+def login():
+    """ login """
+
+    def login_callback(_):
+        """ login_callback """
+
+        def reply_callback(req):
+            req_result = json.loads(req.text)
+            if req.status != 200:
+                if 'message' in req_result:
+                    alert(f"Erreur à la connexion : {req_result['message']}")
+                elif 'msg' in req_result:
+                    alert(f"Problème à la connexion : {req_result['msg']}")
+                else:
+                    alert("Réponse du serveur imprévue et non documentée")
+                return
+            storage['PSEUDO'] = pseudo
+            storage['JWT_TOKEN'] = req_result['AccessToken']
+            time_stamp = time.time()
+            storage['LOGIN_TIME'] = str(time_stamp)
+            InfoDialog("OK", f"Logué avec succès en tant que {pseudo}", remove_after=config.REMOVE_AFTER)
+            show_login()
+
+            render(g_panel_middle)
+
+        pseudo = input_pseudo.value
+        password = input_password.value
+
+        host = config.SERVER_CONFIG['USER']['HOST']
+        port = config.SERVER_CONFIG['USER']['PORT']
+        url = f"{host}:{port}/login"
+
+        json_dict = {
+            'user_name': pseudo,
+            'password': password
+        }
+
+        # login (getting token) : no need for token
+        ajax.post(url, blocking=True, headers={'content-type': 'application/json'}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
+
+    def forgot_callback(_):
+        """ forgot_callback """
+
+        alert("Désolé: la récupération du mot de passe n'est pas encore implémentée - vous pouvez contacter le support (cf. page d'accueil) qui vous forcera un nouveau mot de passe")
+
+        render(g_panel_middle)
+
+    def logout_callback(_):
+        """ logout_callback """
+
+        effective = logout()
+        if effective:
+            InfoDialog("OK", "Délogué avec succès", remove_after=config.REMOVE_AFTER)
+
+        render(g_panel_middle)
+
+    # begins here
+
+    sub_panel = html.DIV(id='sub_panel')
+
+    # --
+    form1 = html.FORM()
+
+    form1 <= html.B("Pas de compte ? Crééz-le à partir du menu 'mon compte'...")
+    form1 <= html.BR()
+    form1 <= html.BR()
+
+    legend_pseudo = html.LEGEND("pseudo")
+    form1 <= legend_pseudo
+
+    proposed_pseudo = ""
+    if 'PSEUDO' in storage:
+        proposed_pseudo = storage['PSEUDO']
+
+    input_pseudo = html.INPUT(type="text", value=proposed_pseudo)
+    form1 <= input_pseudo
+    form1 <= html.BR()
+    form1 <= html.BR()
+
+    legend_password = html.LEGEND("mot de passe")
+    form1 <= legend_password
+    input_password = html.INPUT(type="password", value="")
+    form1 <= input_password
+    form1 <= html.BR()
+    form1 <= html.BR()
+
+    input_login = html.INPUT(type="submit", value="connexion")
+    input_login.bind("click", login_callback)
+    form1 <= input_login
+
+    sub_panel <= form1
+    sub_panel <= html.BR()
+    sub_panel <= html.BR()
+
+    # --
+    form2 = html.FORM()
+
+    input_forgot = html.INPUT(type="submit", value="mot de passe oublié")
+    input_forgot.bind("click", forgot_callback)
+    form2 <= input_forgot
+
+    sub_panel <= form2
+    sub_panel <= html.BR()
+    sub_panel <= html.BR()
+
+    # --
+    form3 = html.FORM()
+
+    input_logout = html.INPUT(type="submit", value="déconnexion")
+    input_logout.bind("click", logout_callback)
+    form3 <= input_logout
+
+    sub_panel <= form3
+
+    return sub_panel
+
+
+def logout():
+    """ logout """
+
+    effective = False
+
+    if 'PSEUDO' in storage:
+        del storage['PSEUDO']
+        effective = True
+
+    if 'LOGIN_TIME' in storage:
+        del storage['LOGIN_TIME']
+        effective = True
+
+    show_login()
+
+    render(g_panel_middle)
+    
+    return effective
+
+
 def check_token():
     """ check_token """
 
@@ -53,125 +193,6 @@ def check_token():
     ajax.get(url, blocking=True, headers={'content-type': 'application/json', 'Authorization': f"Bearer {storage['JWT_TOKEN']}"}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
 
 
-def login_callback(_):
-    """ login_callback """
-
-    def reply_callback(req):
-        req_result = json.loads(req.text)
-        if req.status != 200:
-            if 'message' in req_result:
-                alert(f"Erreur à la connexion : {req_result['message']}")
-            elif 'msg' in req_result:
-                alert(f"Problème à la connexion : {req_result['msg']}")
-            else:
-                alert("Réponse du serveur imprévue et non documentée")
-            return
-        storage['PSEUDO'] = pseudo
-        storage['JWT_TOKEN'] = req_result['AccessToken']
-        time_stamp = time.time()
-        storage['LOGIN_TIME'] = str(time_stamp)
-        InfoDialog("OK", f"Logué avec succès en tant que {pseudo}", remove_after=config.REMOVE_AFTER)
-        show_login()
-
-    pseudo = input_pseudo.value
-    password = input_password.value
-
-    host = config.SERVER_CONFIG['USER']['HOST']
-    port = config.SERVER_CONFIG['USER']['PORT']
-    url = f"{host}:{port}/login"
-
-    json_dict = {
-        'user_name': pseudo,
-        'password': password
-    }
-
-    # login (getting token) : no need for token
-    ajax.post(url, blocking=True, headers={'content-type': 'application/json'}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
-
-
-def forgot_callback(_):
-    """ forgot_callback """
-
-    alert("Désolé: la récupération du mot de passe n'est pas encore implémentée - vous pouvez contacter le support (cf. page d'accueil) qui vous forcera un nouveau mot de passe")
-
-
-def logout():
-    """ logout """
-
-    effective = False
-    if 'PSEUDO' in storage:
-        del storage['PSEUDO']
-        effective = True
-    if 'LOGIN_TIME' in storage:
-        del storage['LOGIN_TIME']
-        effective = True
-    show_login()
-    return effective
-
-
-def logout_callback(_):
-    """ logout_callback """
-    effective = logout()
-    if effective:
-        InfoDialog("OK", "Délogué avec succès", remove_after=config.REMOVE_AFTER)
-
-
-my_panel = html.DIV(id="login")
-
-form1 = html.FORM()
-
-
-form1 <= html.B("Pas de compte ? Crééz-le à partir du menu 'mon compte'...")
-form1 <= html.BR()
-form1 <= html.BR()
-
-legend_pseudo = html.LEGEND("pseudo")
-form1 <= legend_pseudo
-
-PROPOSED_PSEUDO = ""
-if 'PSEUDO' in storage:
-    PROPOSED_PSEUDO = storage['PSEUDO']
-
-input_pseudo = html.INPUT(type="text", value=PROPOSED_PSEUDO)
-form1 <= input_pseudo
-form1 <= html.BR()
-
-legend_password = html.LEGEND("mot de passe")
-form1 <= legend_password
-input_password = html.INPUT(type="password", value="")
-form1 <= input_password
-form1 <= html.BR()
-
-form1 <= html.BR()
-
-input_login = html.INPUT(type="submit", value="connexion")
-input_login.bind("click", login_callback)
-form1 <= input_login
-form1 <= html.BR()
-
-my_panel <= form1
-my_panel <= html.BR()
-
-form2 = html.FORM()
-
-input_forgot = html.INPUT(type="submit", value="mot de passe oublié")
-input_forgot.bind("click", forgot_callback)
-form2 <= input_forgot
-form2 <= html.BR()
-
-my_panel <= form2
-my_panel <= html.BR()
-
-form3 = html.FORM()
-
-input_logout = html.INPUT(type="submit", value="déconnexion")
-input_logout.bind("click", logout_callback)
-form3 <= input_logout
-form3 <= html.BR()
-
-my_panel <= form3
-
-
 def show_login():
     """  show_login """
 
@@ -198,7 +219,23 @@ def show_login():
     document <= show_login_panel
 
 
+g_panel_middle = None  # pylint: disable=invalid-name
+
+
 def render(panel_middle):
     """ render """
+
+    global g_panel_middle  # pylint: disable=invalid-name
+    g_panel_middle = panel_middle
+
+    my_panel.clear()
+
+    my_sub_panel = login()
+
+    if my_sub_panel:
+        my_panel <= html.B("Identifiez vous pour accéder aux ressources protégées")
+        my_panel <= html.BR()
+        my_panel <= html.BR()
+        my_panel <= my_sub_panel
 
     panel_middle <= my_panel
