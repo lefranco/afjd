b9e39083341a35ed1cd86484b7ce367992a01924
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-11-28 16:45:24 +0100

    .

--- client-ihms-brython/center_design.py
+++ client-ihms-brython/center_design.py
@@ -4,10 +4,11 @@
 
 import math
 
+CENTER_RAY = 5
 
 def stabbeur_center(x: int, y: int, ctx):  # pylint: disable=invalid-name
     """ display a center the stabbeur way """
 
     ctx.beginPath()
-    ctx.arc(x, y, 4, 0, 2 * math.pi, False)
+    ctx.arc(x, y, CENTER_RAY, 0, 2 * math.pi, False)
     ctx.fill(); ctx.stroke(); ctx.closePath()
