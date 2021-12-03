ec1994a60b633a032de502d775360e36d944f4bd
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2021-12-03 19:07:13 +0100

    .

--- client-ihms-brython/home.py
+++ client-ihms-brython/home.py
@@ -2,12 +2,17 @@
 
 # pylint: disable=pointless-statement, expression-not-assigned
 
-from browser import html  # pylint: disable=import-error
+from browser import html, alert  # pylint: disable=import-error
+from browser.widgets.dialog import InfoDialog  # pylint: disable=import-error
+from browser.local_storage import storage  # pylint: disable=import-error
 
+import faq
+import interface
+import config
 import common
 
 
-OPTIONS = ['accueil', 'liens', 'support', 'foire aux question', 'coin technique']
+OPTIONS = ['accueil', 'liens', 'support', 'foire aux question', 'coin technique', 'choix d\'interface']
 
 
 def show_home():
@@ -103,62 +108,8 @@ def show_support():
     my_sub_panel <= text3
 
 
-faq_content_table = {
-
-    "Pourquoi certaines messages sont en français, certains en angals, voire certains dans un mélange des deux  ?":
-    "Les messages issus de l'interface 'front end' sont en français (sauf omission à corriger rapidement). Les messages issus du serveur 'back end' sont en anglais",
-
-    "Peut-on jouer plusieurs rôles sur une partie  ?":
-    "Non, cela n'est pas prévu. Il doit y avoir 8 intervenants distincts sur une partie (arbitre y compris)",
-
-    "Peut-on faire des erreurs d'ordres ?":
-    "Eh non. Les ordres sont 100% vérifiés avant d'être enregistrés. Par contre le menu 'taguer' dans une partie permet une communcation par ordres 'ésotériques'. Cela ne presente un intérêt que dans les parties sans communication",
-
-    "Quand a lieu la résolution ?":
-    "Quand le dernier joueur qui a des ordres a rendre coche sur la case 'prêt à résoudre' tout simplement (ou quand l'arbitre le fait à sa place pour éviter que la partie ne s'éternise)",
-
-    "Pourquoi les dates limites changent-elles de couleur ?":
-    "Le code de couleur est asez conventionnel. Jaune signifie que la date limite est proche (24h). Orange qu'elle est passée. Rouge que la grâce est aussi passée. Soyez ponctuels !",
-
-    "Il y a des retards indiqués. Dans quel cas un joueur est -il marqué en retard ?":
-    "Passer ses ordres signifie réaliser une transition 'pas prêt à résoudre' (ou pas d'information) -> 'prêt à résoudre'. Un retard signifie que cela est réalisé après la date limite (que ce soit par le joueur ou par l'arbitre). Si l'arbitre reporte la date limite, le retard n'est pas effacé (mais il est impossible d'avoir deux retards sur une même saison.) Soyez ponctuels !",
-
-    "Pourquoi les unités ne se déplacent pas dans le bac à sable ?":
-    "Le bac à sable n'a pas vocation à déplacer les unités, il cherche à répondre à la question 'que se passerait-il si...'. Il faut bien lire le compte rendu des ordres sous forme de texte sous la carte",
-
-    "Eh, je pense avoir trouvé un bug dans le moteur de résolution. Que faire ?":
-    "Le moteur s'appuie sur le DATC. Il faut utiliser le lien en page d'accueil pour envoyer un e-mail et signaler le problème. Reproduisez le soigneusement avec le bac à sable",
-
-    "Où trouver des explications sur les paramètres des parties ?":
-    "Soit en survolant les titres à la creation de la partie, soit en consultant le menu 'paramètres' d'une partie existante",
-
-    "Comment remplacer un joueur ?":
-    "C'est le rôle de l'arbitre. Il faut lui retirer le rôle dans la console d'arbitrage et attribuer le rôle à un autre joueur dans la partie, qu'il aura fallu faire venir au préalable. Faire venir ou partir les joueurs de la partie se réalise par contre dans le menu appariement",
-
-    "Que peut faire l'arbitre ?":
-    "L'arbitre démarre et arrête la partie. L'arbitre force une résolution quand il n'y a pas besoin d'ordres. Il peut forcer des ordres de désordre civil pour un pays. Il peut forcer un accord pour résoudre pour un pays. Il peut retirer ou ajouter un joueur dans une partie, et allouer un rôle ou retirer un rôle à un joueur dans une partie. Il peut modifier une date limite (même si celle-ci est gérée par le système). Il gère également les paramètres de la partie.",
-
-    "Comment créér une partie dans laquelle je joue ?":
-    "Il faut créer la partie, puis se retirer de l'arbitrage et demander à un arbitre de parties du site d'en prendre l'arbitrage",
-
-    "C'est quoi ce truc : l'oracle ?":
-    "Pas encore développé. Fournira une suggestion de jeux d'ordres à partir d'une position...",
-
-    "Est-il prévu de développer des variantes ?":
-    "Certainement. Hundred tient la corde pour le moment",
-
-    "Je veux donner de l'argent pour le site. Que faire ?":
-    "Reportez vous à la page d'accueil du site pour le lien vers le site de recueil des dons",
-
-    "Comment discuter avec les membres de la communauté":
-    "Reportez vous à la page d'accueil du site pour le lien vers la brique sociale.",
-
-    "Ce site est-il le site de jeu officiel de l'AFJD ?":
-    "Oui. Une autre interface moins rustique est prévue mais plus tard.",
-}
-
-faq_displayed_table = {k: False for k in faq_content_table}
-faq_content = html.DIV("faq")
+faq_displayed_table = {k: False for k in faq.FAQ_CONTENT_TABLE}  # pylint: disable=invalid-name
+faq_content = html.DIV("faq")  # pylint: disable=invalid-name
 
 
 def show_faq():
@@ -175,7 +126,7 @@ def show_faq():
 
     faq_content.clear()
 
-    for question_txt, answer_txt in faq_content_table.items():
+    for question_txt, answer_txt in faq.FAQ_CONTENT_TABLE.items():
 
         reveal_button = html.INPUT(type="submit", value=question_txt)
         reveal_button.bind("click", lambda e, q=question_txt: reveal_callback(e, q))
@@ -254,6 +205,82 @@ def show_technical():
     my_sub_panel <= iframe2
 
 
+def select_interface():
+    """ select_interface """
+
+    variant_name_loaded = None
+
+    def select_interface_callback(_, user_interface):
+        """ select_interface_callback """
+
+        interface.set_interface(variant_name_loaded, user_interface)
+        InfoDialog("OK", f"Interface sélectionnée pour la variante {variant_name_loaded} : {user_interface}", remove_after=config.REMOVE_AFTER)
+
+        # back to where we started
+        my_sub_panel.clear()
+        select_interface()
+
+    title1 = html.H3("Choix d'interface")
+    my_sub_panel <= title1
+
+    if 'GAME' not in storage:
+        alert("Il faut choisir la partie au préalable (pour la variante)")
+        return
+
+    game = storage['GAME']
+
+    # from game name get variant name
+
+    variant_name_loaded = common.game_variant_name_reload(game)
+    if not variant_name_loaded:
+        alert("Problème chargement nom de variante")
+        return
+
+    information = html.DIV(Class='important')
+    information <= "Une 'interface' vous permet d'avoir une carte et des trigrammes de désignation des régions sépcifiquaes différents de ceux pratiqués sur le site"
+    my_sub_panel <= information
+    my_sub_panel <= html.BR()
+
+    select_table = html.TABLE()
+
+    for user_interface in interface.INTERFACE_TABLE[variant_name_loaded]:
+
+        # get description
+        with open(f"./variants/{variant_name_loaded}/{user_interface}/README", "r") as file_ptr:
+            lines = file_ptr.readlines()
+        description = html.DIV(Class='note')
+        for line in lines:
+            description <= line
+            description <= html.BR()
+
+        form = html.FORM()
+        fieldset = html.FIELDSET()
+        legend_display = html.LEGEND(user_interface, title=description)
+        fieldset <= legend_display
+        form <= fieldset
+
+        fieldset = html.FIELDSET()
+        fieldset <= description
+        form <= fieldset
+
+        form <= html.BR()
+
+        input_select_interface = html.INPUT(type="submit", value="sélectionner cette interface")
+        input_select_interface.bind("click", lambda e, i=user_interface: select_interface_callback(e, i))
+        form <= input_select_interface
+
+        col = html.TD()
+        col <= form
+        col <= html.BR()
+
+        row = html.TR()
+        row <= col
+
+        select_table <= row
+
+    my_sub_panel <= select_table
+
+
 my_panel = html.DIV()
 my_panel.attrs['style'] = 'display: table-row'
 
@@ -286,6 +313,8 @@ def load_option(_, item_name):
         show_faq()
     if item_name == 'coin technique':
         show_technical()
+    if item_name == 'choix d\'interface':
+        select_interface()
 
     global item_name_selected  # pylint: disable=invalid-name
     item_name_selected = item_name
