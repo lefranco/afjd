5f9bb7a49bb547d961db1654e0273977e56e314e
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-12-04 19:25:57 +0100

    .

--- client-ihms-brython/tournament.py
+++ client-ihms-brython/tournament.py
@@ -3,10 +3,91 @@
 # pylint: disable=pointless-statement, expression-not-assigned
 
 
-from browser import html, ajax, alert, window  # pylint: disable=import-error
+from browser import html, alert, window  # pylint: disable=import-error
+from browser.local_storage import storage  # pylint: disable=import-error
 
+import common
 
 OPTIONS = ['créer les parties']
+
+
+def perform_batch(games_to_create):
+    """ perform_batch """
+
+    alert("Ok, on tente le truc..")
+
+    games_dict = common.get_games_data()
+    if not games_dict:
+        alert("Erreur chargement dictionnaire parties")
+        return
+    games_dict = dict(games_dict)
+    #  print(f"{games_dict=}")
+    games_set = {d['name'] for d in games_dict.values()}
+
+    players_dict = common.get_players_data()
+    if not players_dict:
+        alert("Erreur chargement dictionnaire joueurs")
+        return
+    players_dict = dict(players_dict)
+    #  print(f"{players_dict=}")
+    players_set = {d['pseudo'] for d in players_dict.values()}
+
+    # check the game does not exist
+    for ligne, game_name in enumerate(games_to_create):
+
+        if not game_name:
+            alert(f"Il y a un nom de partie vide dans le fichier en ligne {ligne+1}")
+            error = True
+
+        if game_name in games_set:
+            alert(f"Il semble que la partie {game_name} existe déjà")
+            error = True
+
+    # check the players exist
+    already_warned = set()
+    for ligne, allocations in enumerate(games_to_create.values()):
+        for player_name in allocations.values():
+
+            if not player_name:
+                alert(f"Il y a un nom de joueur vide dans le fichier en ligne {ligne+1}")
+                error = True
+
+            if player_name not in players_set:
+
+                # patch
+                if player_name.startswith("Joueur"):
+                    continue
+
+                if player_name not in already_warned:
+                    alert(f"Il semble que le pseudo {player_name} n'existe pas")
+                    already_warned.add(player_name)
+                    error = True
+
+    # check the roles are complete
+    for game_name, allocations in games_to_create.items():
+        if sorted(allocations.keys()) != list(range(8)):
+            alert(f"Il semble que la partie {game_name} n'a pas ses 8 joueurs")
+            error = True
+
+    # check the players in games are not duplicated
+    for game_name, allocations in games_to_create.items():
+        if len(set(allocations.keys())) != 8:
+            alert(f"Il semble que la partie {game_name} n'a pas 8 joueurs différents")
+            error = True
+
+    if 'PSEUDO' not in storage:
+        alert("Il faut se connecter pour créer les parties")
+        return
+
+    pseudo = storage['PSEUDO']
+
+    # check the game master is pseudo (to check last)
+    for game_name, allocations in games_to_create.items():
+        if allocations[0] != pseudo:
+            alert(f"Il semble que vous ne soyez pas l'arbitre créateur de la partie {game_name}. Je ne pourrais donc pas la crééer. ")
+
+    if not error:
+        alert("Mouais. Ca pourrait être jouable ton truc ;-)")
 
 
 def create_games():
@@ -23,6 +104,10 @@ def create_games():
             content = str(reader.result)
             lines = content.split('\n')
 
+            if not len(lines) >= 1:
+                alert("Votre fichier n'a pas de lignes")
+                return
+
             for line in lines:
 
                 # ignore empty lines
@@ -30,16 +115,17 @@ def create_games():
                     continue
 
                 tab = line.split(',')
-                game_name = tab[0]
 
-                # TODO : check = pseudo
-                game_master = tab[1]
+                if not len(tab) >= 2:
+                    alert("Votre fichier n'est pas un csv")
+                    return
+
+                game_name = tab[0]
 
                 games_to_create[game_name] = {n: tab[n + 1] for n in range(len(tab) - 1)}
 
-            alert(f"{games_to_create=}")
-
-            # TODO : actual creation of all the games
+            #  actual creation of all the games
+            perform_batch(games_to_create)
 
             # back to where we started
             my_sub_panel.clear()
@@ -59,8 +145,6 @@ def create_games():
         # Read the file content as text
         reader.readAsBinaryString(file)
         reader.bind("load", onload_callback)
-
-        alert("creation des parties non opérationnelle pour le moment")
 
         # back to where we started
         my_sub_panel.clear()
