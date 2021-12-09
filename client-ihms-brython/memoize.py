e28539f1d7d2e49544785a41a464b3077cc8ac39
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2021-12-09 10:45:24 +0100

    .

--- client-ihms-brython/memoize.py
+++ client-ihms-brython/memoize.py
@@ -2,18 +2,12 @@
 
 # pylint: disable=pointless-statement, expression-not-assigned
 
-#  from browser.local_storage import storage  # pylint: disable=import-error
-
 
 class MemoizeTable:
     """ MemoizeTable :  uses storage to share a dictionary """
 
     def __init__(self):
         self._data = dict()
-
-    def keys(self):
-        """ the keys() method of a dict """
-        return self._data.keys()
 
     def __contains__(self, key):
         return key in self._data
@@ -24,10 +18,12 @@ class MemoizeTable:
     def __setitem__(self, key, value):
         self._data[key] = value
 
-    def __delitem__(self, key):
-        del self._data[key]
 
-
-VARIANT_DATA_MEMOIZE_TABLE = MemoizeTable()
-PARAMETERS_READ_MEMOIZE_TABLE = MemoizeTable()
+# using this avoids a server download (costy on slow connections)
 VARIANT_CONTENT_MEMOIZE_TABLE = MemoizeTable()
+
+# using this avoids a read from file (costy on slow computers)
+PARAMETERS_READ_MEMOIZE_TABLE = MemoizeTable()
+
+# using this avoids an object creation (always costy)
+VARIANT_DATA_MEMOIZE_TABLE = MemoizeTable()
