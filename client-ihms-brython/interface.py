48f315da3192d7248bc38c989e1c799808f3ca92
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-12-22 21:07:42 +0100

    .

--- client-ihms-brython/interface.py
+++ client-ihms-brython/interface.py
@@ -7,7 +7,7 @@ from browser.local_storage import storage  # pylint: disable=import-error
 
 # simplest is to hard code displays of variants here
 INTERFACE_TABLE = {
-    'standard': ['diplomania', 'diplomatie_online', '18_centres', 'stabbeurfou']
+    'standard': ['diplomania', 'Diplomatie-online', '18centres', 'stabbeurfou']
 }
 
 
