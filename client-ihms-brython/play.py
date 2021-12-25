cc9a2e823537ae4ac26916251d01dfad58461fc4
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2021-12-25 11:57:45 +0100

    .

--- client-ihms-brython/play.py
+++ client-ihms-brython/play.py
@@ -3259,9 +3259,70 @@ def game_master():
 
         subject = f"Message de la part de l'arbitre de la partie {GAME} sur le site https://diplomania-gen.fr (AFJD)"
 
+        role = VARIANT_DATA.roles[role_id]
+        role_name = VARIANT_DATA.name_table[role]
+
         body = "Bonjour !"
         body += "\n"
-        body += "Il manque vos ordres et la date limite est passée. Merci d'aviser rapidement."
+        body += "Il manque vos ordres et la date limite est passée. Merci d'aviser rapidement !"
+        body += "\n"
+        body += f"Pour rappel votre rôle est {role_name}."
+        body += "\n"
+        body += "Pour se rendre directement sur la partie :\n"
+        body += f"https://diplomania-gen.fr?game={GAME}"
+
+        player_id_str = role2pseudo[role_id]
+        player_id = int(player_id_str)
+        pseudo_there = id2pseudo[player_id]
+
+        addressed_id = PLAYERS_DICT[pseudo_there]
+        addressees = [addressed_id]
+
+        json_dict = {
+            'pseudo': PSEUDO,
+            'addressees': " ".join([str(a) for a in addressees]),
+            'subject': subject,
+            'body': body,
+            'force': True,
+        }
+
+        host = config.SERVER_CONFIG['PLAYER']['HOST']
+        port = config.SERVER_CONFIG['PLAYER']['PORT']
+        url = f"{host}:{port}/mail-players"
+
+        # sending email : need token
+        ajax.post(url, blocking=True, headers={'content-type': 'application/json', 'AccessToken': storage['JWT_TOKEN']}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
+
+
+    def send_welcome_email_callback(_, role_id):
+        """ send_welcome_email_callback """
+
+        pseudo_there = None
+
+        def reply_callback(req):
+            nonlocal pseudo_there
+            req_result = json.loads(req.text)
+            if req.status != 200:
+                if 'message' in req_result:
+                    alert(f"Erreur à l'envoi de courrier électronique message de bienvenue : {req_result['message']}")
+                elif 'msg' in req_result:
+                    alert(f"Problème à l'envoi de courrier électronique message de bienvenue: {req_result['msg']}")
+                else:
+                    alert("Réponse du serveur imprévue et non documentée")
+                return
+
+            InfoDialog("OK", f"Message de bienvenue émis vers : {pseudo_there}", remove_after=config.REMOVE_AFTER)
+
+        subject = f"Message de la part de l'arbitre de la partie {GAME} sur le site https://diplomania-gen.fr (AFJD)"
+
+        role = VARIANT_DATA.roles[role_id]
+        role_name = VARIANT_DATA.name_table[role]
+
+        body = "Bonjour !"
+        body += "\n"
+        body += "J'ai l'immense honneur de vous informer que vous avez été mis dans la partie et pouvez donc commencer à jouer !"
+        body += "\n"
+        body += f"Le rôle qui vous a été attribué est {role_name}."
         body += "\n"
         body += "Pour se rendre directement sur la partie :\n"
         body += f"https://diplomania-gen.fr?game={GAME}"
@@ -3663,6 +3724,15 @@ def game_master():
             else:
                 flag = html.IMG(src="./images/continue.jpg", title="Continuer la partie")
         col <= flag
+        row <= col
+
+        col = html.TD()
+        input_send_welcome_email = ""
+        if role_id in needed_roles_list:
+            if role_id not in submitted_roles_list:
+                input_send_welcome_email = html.INPUT(type="submit", value="courriel de bienvenue")
+                input_send_welcome_email.bind("click", lambda e, r=role_id: send_welcome_email_callback(e, r))
+        col <= input_send_welcome_email
         row <= col
 
         game_admin_table <= row
