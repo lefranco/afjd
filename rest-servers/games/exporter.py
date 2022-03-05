108555bb48f1fca1e7b5cd74ce11b61cd0ce225d
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2022-03-05 10:46:13 +0100

    .

--- rest-servers/games/exporter.py
+++ rest-servers/games/exporter.py
@@ -17,6 +17,7 @@ import lowdata
 import database
 import games  # noqa: F401 pylint: disable=unused-import
 import ownerships
+import units
 import transitions
 import groupings
 import tournaments
@@ -37,7 +38,8 @@ assert len(POWER_NAME) == 7, "Bad number of powers"
 CENTER_NAME = [
     'Ank', 'Bel', 'Ber', 'Bre', 'Bud', 'Bul', 'Con', 'Den', 'Edi', 'Gre', 'Hol', 'Kie', 'Lon', 'Lvp', 'Mar', 'Mos',
     'Mun', 'Nap', 'Nwy', 'Par', 'Por', 'Rom', 'Rum', 'Ser', 'Sev', 'Smy', 'Spa', 'Stp', 'Swe', 'Tri', 'Tun', 'Ven',
-    'Vie', 'War']
+    'Vie', 'War'
+]
 assert len(CENTER_NAME) == 34, "Bad number of centers"
 
 TYPE_NAME = ['A', 'F']
@@ -49,7 +51,8 @@ ZONE_NAME = [
     'Kie', 'Lon', 'Lvn', 'Lvp', 'Mar', 'MAO', 'Mos', 'Mun', 'Naf', 'Nap', 'NAO', 'NWG', 'NTH', 'Nwy', 'Par', 'Pic',
     'Pie', 'Por', 'Pru', 'Rom', 'Ruh', 'Rum', 'Ser', 'Sev', 'Sil', 'SKA', 'Smy', 'Spa', 'Stp', 'Swe', 'Syr', 'Tri',
     'Tun', 'Tus', 'TYS', 'Tyr', 'Ukr', 'Ven', 'Vie', 'Wal', 'War', 'WES', 'Yor',
-    ['Bul', 'ec'], ['Bul', 'sc'], ['Spa', 'nc'], ['Spa', 'sc'], ['Stp', 'nc'], ['Stp', 'sc']]
+    ['Bul', 'ec'], ['Bul', 'sc'], ['Spa', 'nc'], ['Spa', 'sc'], ['Stp', 'nc'], ['Stp', 'sc']
+]
 assert len(ZONE_NAME) == 81, "Bad number of zones"
 
 FRENCH_ZONE_NAME = [
@@ -58,14 +61,41 @@ FRENCH_ZONE_NAME = [
     'KIE', 'LON', 'LVN', 'LVP', 'MAR', 'ATL', 'MOS', 'MUN', 'AFN', 'NAP', 'ATN', 'MNG', 'NRD', 'NGE', 'PAR', 'PIC',
     'PIE', 'POR', 'PRU', 'ROM', 'RUH', 'ROU', 'SER', 'SEB', 'SIL', 'SKA', 'SMY', 'ESP', 'STP', 'SUE', 'SYR', 'TRI',
     'TUN', 'TOS', 'MTY', 'TYR', 'UKR', 'VEN', 'VIE', 'PGA', 'VAR', 'MOC', 'YOR',
-    'BULce', 'BULcs', 'ESPcn', 'ESPcs', 'STPcn', 'STPcs']
+    'BULce', 'BULcs', 'ESPcn', 'ESPcs', 'STPcn', 'STPcs'
+]
 assert len(FRENCH_ZONE_NAME) == 81, "Bad number of french zones"
 
 # TODO : remove this at some point
 # for old games, sometimes some other codes were used
-TRANSLATE = {'ODE': 'SEB', 'TOU': 'MAR', 'CAU': 'ARM', 'PAY': 'HOL', 'COR': 'PGA', 'RHE': 'RUH', 'ALP': 'TYR', 'LIO': 'GLI', 'STG': 'IRL', 'NRG': 'MNG', 'NWY': 'NGE', 'AEG': 'EGE'}
-assert not set(TRANSLATE.keys()) & set(FRENCH_ZONE_NAME), "Translator : a key is a standard french name"
-assert set(TRANSLATE.values()) <= set(FRENCH_ZONE_NAME), "Translator : a value is not a standard french name"
+OLD_CODES_CONVERSION_TABLE = {
+    'ODE': 'SEB', 'TOU': 'MAR', 'CAU': 'ARM', 'PAY': 'HOL', 'COR': 'PGA', 'RHE': 'RUH', 'ALP': 'TYR', 'LIO': 'GLI', 'STG': 'IRL', 'NRG': 'MNG', 'NWY': 'NGE', 'AEG': 'EGE'
+}
+assert not set(OLD_CODES_CONVERSION_TABLE.keys()) & set(FRENCH_ZONE_NAME), "Translator : a key is a standard french name"
+assert set(OLD_CODES_CONVERSION_TABLE.values()) <= set(FRENCH_ZONE_NAME), "Translator : a value is not a standard french name"
+
+
+SCORING_CONVERSION_TABLE = {
+    'CDIP': 'CDiplo',
+    'WNAM': 'WinNamur',
+    'DLIG': 'DiploLigue',
+    'NOMG': 'OpenMindTheGap',
+    'CNAM': 'CDiploNamur'
+}
+
+
+def advancement2game_season_year(advancement: int) -> typing.Tuple[int, int]:
+    """" advancement2game_season_year """
+
+    game_year = 1901 + advancement // 5
+
+    if advancement % 5 in [0, 1]:
+        game_season = 1
+    elif advancement % 5 in [2, 3]:
+        game_season = 2
+    elif advancement % 5 == 4:
+        game_season = 3
+
+    return game_season, game_year
 
 
 def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bool) -> typing.Tuple[bool, str, typing.Optional[typing.Dict[str, typing.Any]]]:
@@ -116,7 +146,7 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
 
     # scoring system
     game_scoring = game.scoring
-    result['ScoringSystem'] = game_scoring
+    result['ScoringSystem'] = SCORING_CONVERSION_TABLE[game_scoring]
 
     # communication
     if game.nomessage_game and game.nopress_game:
@@ -158,7 +188,7 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
     if not allocations_found:
         return False, "Internal error : Did not find allocations for game", None
     result['Players'] = {}
-    for _, player_id, role_id in allocations_found:
+    for _, player_id, role_id in sorted(allocations_found, key=lambda p: p[2]): # type: ignore
         if not role_id > 0:
             continue
         role_num = role_id - 1
@@ -226,24 +256,19 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
         transitions_found += 1
 
         the_situation = json.loads(transition.situation_json)
+
         ownership_dict = the_situation['ownerships']
         unit_dict = the_situation['units']
+        dislodged_unit_dict = the_situation['dislodged_ones']
         the_orders = json.loads(transition.orders_json)
 
-        # game year : easy
-        game_year = 1901 + advancement // 5
+        # extract year and season
+        game_season, game_year = advancement2game_season_year(advancement)
 
         # season : not so easy
         if advancement % 5 in [0, 2, 4]:
 
             phase_data: typing.Dict[str, typing.Any] = {}
-
-            if advancement % 5 == 0:
-                game_season = 1
-            elif advancement % 5 == 2:
-                game_season = 2
-            elif advancement % 5 == 4:
-                game_season = 3
 
             phase_data['Phase'] = game_year * 10 + game_season
             phase_data['Status'] = 'Completed'
@@ -269,8 +294,14 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
             units_phase = {}
             for role_num, power_name in enumerate(POWER_NAME):
                 role_id = role_num + 1
+
                 units_phase[power_name] = [[TYPE_NAME[int(t) - 1], ZONE_NAME[int(z) - 1]] for t, z in unit_dict[str(POWER_NAME.index(power_name) + 1)]] if str(POWER_NAME.index(power_name) + 1) in unit_dict else []
+
             phase_data['Units'] = units_phase
+
+        if advancement % 5 in [1, 3]:
+            dislodged_units_phase = [ZONE_NAME[int(z) - 1] for dislodged in dislodged_unit_dict.values() for _, z, _ in dislodged]
+            phase_data['DislodgedUnits'] = dislodged_units_phase
 
         # this is text information
         report_txt = transition.report_txt
@@ -293,23 +324,23 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
                 french_unit = words[1]
                 if french_unit not in FRENCH_ZONE_NAME:
                     # perform translation using little table
-                    french_unit = TRANSLATE[french_unit]
+                    french_unit = OLD_CODES_CONVERSION_TABLE[french_unit]
                 zone_num = FRENCH_ZONE_NAME.index(french_unit) + 1
                 _, _, justification = line.partition(';')
                 justification_table[zone_num] = justification
 
-        if advancement % 5 in [0, 2, 4]:
-            # remember stuff from moves for retreats
-            orders_table = {}
-
         # now extract all the orders
 
         orders_phase: typing.Dict[str, typing.Any] = {}
+        orders_phase_retreats: typing.Dict[str, typing.Any] = {}
         actual_orders_list = the_orders['orders']
         fake_units_list = the_orders['fake_units']
         for role_num, power_name in enumerate(POWER_NAME):
+
             role_id = role_num + 1
+
             orders_phase[power_name] = []
+            orders_phase_retreats[power_name] = []
 
             # build the table fake units zone -> type
             fake_table = {}
@@ -333,64 +364,60 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
                     if adj_justif:
                         adj_data.extend([adj_justif])
                     order_description = [active_design, ['m', ZONE_NAME[int(destination) - 1]], adj_data]
-                    orders_table[active_design] = order_description
+                    orders_phase[power_name].append(order_description)
                 if order == 2:  # attack support
                     adj_data = [adj_result]
                     if adj_justif:
                         adj_data.extend([adj_justif])
                     order_description = [active_design, ['sm', ZONE_NAME[int(passive) - 1], ZONE_NAME[int(destination) - 1]], adj_data]
-                    orders_table[active_design] = order_description
+                    orders_phase[power_name].append(order_description)
                 if order == 3:  # attack support
                     adj_data = [adj_result]
                     if adj_justif:
                         adj_data.extend([adj_justif])
                     order_description = [active_design, ['sh', ZONE_NAME[int(passive) - 1]], adj_data]
-                    orders_table[active_design] = order_description
+                    orders_phase[power_name].append(order_description)
                 if order == 4:  # hold
                     adj_data = [adj_result]
                     if adj_justif:
                         adj_data.extend([adj_justif])
                     order_description = [active_design, ['h'], adj_data]
-                    orders_table[active_design] = order_description
+                    orders_phase[power_name].append(order_description)
                 if order == 5:  # convoy
                     adj_data = [adj_result]
                     if adj_justif:
                         adj_data.extend([adj_justif])
                     order_description = [active_design, ['c', ZONE_NAME[int(passive) - 1], ZONE_NAME[int(destination) - 1]], adj_data]
-                    orders_table[active_design] = order_description
+                    orders_phase[power_name].append(order_description)
 
                 if order == 6:  # retreat
-                    # we need to complete previously created element
-                    order_description = orders_table[active_design]
-                    adj_data = [adj_result]
-                    if adj_justif:
-                        adj_data.extend([adj_justif])
-                    order_description.extend([['m', ZONE_NAME[int(destination) - 1]], adj_data])
+                    adj_data2 = adj_result
+                    order_description = [active_design, ['m', ZONE_NAME[int(destination) - 1]], adj_data2]
+                    orders_phase_retreats[power_name].append(order_description)
                 if order == 7:  # disband
-                    # we need to complete previously created element
-                    order_description = orders_table[active_design]
-                    adj_data = [adj_result]
-                    if adj_justif:
-                        adj_data.extend([adj_justif])
-                    order_description.extend([['d'], adj_data])
+                    adj_data2 = adj_result
+                    order_description2 = [active_design, 'd', adj_data2]
+                    orders_phase_retreats[power_name].append(order_description2)
 
                 if order == 8:  # build
                     type_ = fake_table[active]
-                    adj_data = [adj_result]
-                    if adj_justif:
-                        adj_data.extend([adj_justif])
-                    order_description = [active_design, ['b', TYPE_NAME[int(type_) - 1]], adj_data]
+                    adj_data2 = adj_result
+                    order_description3 = [active_design, ['b', [TYPE_NAME[int(type_) - 1], active_design]], adj_data2]
+                    orders_phase[power_name].append(order_description3)
                 if order == 9:  # remove
-                    adj_data = [adj_result]
-                    if adj_justif:
-                        adj_data.extend([adj_justif])
-                    order_description = [active_design, ['d'], adj_data]
+                    adj_data2 = adj_result
+                    order_description2 = [active_design, 'd', adj_data2]
+                    orders_phase[power_name].append(order_description2)
 
-                if advancement % 5 in [0, 2, 4]:
-                    orders_phase[power_name].append(order_description)
+        if advancement % 5 in [0, 2]:
+            phase_data['Orders'] = orders_phase
+            result['GamePhases'].append(phase_data)
 
         # retreat phases do not exist actually
-        if advancement % 5 in [0, 2, 4]:
+        if advancement % 5 in [1, 3]:
+            phase_data['RetreatOrders'] = orders_phase_retreats
+
+        if advancement % 5 in [4]:
             phase_data['Orders'] = orders_phase
             result['GamePhases'].append(phase_data)
 
@@ -406,10 +433,52 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
         if result['ResultSummary'][power_name]['YearOfElimination'] is None and not ratings[power_name]:
             result['ResultSummary'][power_name]['YearOfElimination'] = last_year
 
-    phase_data = result['GamePhases'][-1]
+    # add a last phase
+
+    last_phase_data: typing.Dict[str, typing.Any] = {}
+
+    # extract year and season
+    game_season, game_year = advancement2game_season_year(advancement)
+
+    last_phase_data['Phase'] = game_year * 10 + game_season
 
     # mark the last phase as 'Game Ended'
-    phase_data['Status'] = 'GameEnded'
+    last_phase_data['Status'] = 'GameEnded'
+
+    game_ownerships = ownerships.Ownership.list_by_game_id(sql_executor, game_id)
+    if not game_ownerships:
+        return False, "Internal error : Did not find ownerships for game", None
+    ownership_dict = {}
+    for _, center_num, role_id in game_ownerships:
+        ownership_dict[center_num] = role_id
+
+    # how many centers every power has
+    center_table = {}
+    for role_num, power_name in enumerate(POWER_NAME):
+        role_id = role_num + 1
+        n_centers = len([_ for c, r in ownership_dict.items() if r == role_id])
+        center_table[power_name] = n_centers
+
+    last_phase_data['CenterCounts'] = {}
+    for role_num, power_name in enumerate(POWER_NAME):
+        last_phase_data['CenterCounts'][power_name] = ratings[power_name]
+
+    centers_phase = {}
+    for role_num, power_name in enumerate(POWER_NAME):
+        role_id = role_num + 1
+        centers_phase[power_name] = [CENTER_NAME[int(c) - 1] for c, r in ownership_dict.items() if r == role_id]
+    last_phase_data['SupplyCenters'] = centers_phase
+
+    last_phase_data['Units'] = {}
+    for role_num, power_name in enumerate(POWER_NAME):
+        last_phase_data['Units'][power_name] = []
+
+    game_units = units.Unit.list_by_game_id(sql_executor, game_id)
+    if not game_units:
+        return False, "Internal error : Did not find units for game", None
+    for _, type_unit, zone_unit, role_id, _, _ in game_units:
+        power_name = POWER_NAME[role_id - 1]
+        last_phase_data['Units'][power_name].append([TYPE_NAME[int(type_unit) - 1], ZONE_NAME[int(zone_unit) - 1]])
 
     # extract votes at the end
     game_votes_list = votes.Vote.list_by_game_id(sql_executor, int(game_id))
@@ -423,7 +492,9 @@ def export_data(game_id: int, sql_executor: database.SqlExecutor, debug_mode: bo
     if all(votes_dict[p] for p in POWER_NAME if ratings[p]):
         votes_phase = {}
         votes_phase['Passed'] = True
-        phase_data['DrawVote'] = votes_phase
+        last_phase_data['DrawVote'] = votes_phase
+
+    result['GamePhases'].append(last_phase_data)
 
     return True, f"Game with id '{game_id}' and name '{game.name}' was exported successfully!", result
 
@@ -434,12 +505,12 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--identifier', required=True, help='Id (number) of game')
     parser.add_argument('-d', '--debug_mode', required=False, action='store_true', help='Debug mode : do not load players')
-    parser.add_argument('-J', '--json_output', required=False, help='Output json file')
+    parser.add_argument('-o', '--output_json', required=False, help='Output json file')
     args = parser.parse_args()
 
     game_id = args.identifier
     debug_mode = args.debug_mode
-    json_output = args.json_output
+    output_json = args.output_json
 
     lowdata.load_servers_config()
 
@@ -455,9 +526,9 @@ def main() -> None:
         sys.exit(1)
 
     # output
-    if json_output is not None:
+    if output_json is not None:
         output = json.dumps(result, indent=4, ensure_ascii=False)
-        with open(json_output, 'w', encoding='utf-8') as file_ptr:
+        with open(output_json, 'w', encoding='utf-8') as file_ptr:
             file_ptr.write(output)
 
 
