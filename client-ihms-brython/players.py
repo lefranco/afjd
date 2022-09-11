83fda4ee0b9ebcd5525767f860e0b0f97357eba8
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2022-09-11 17:12:52 +0200

    .

--- client-ihms-brython/players.py
+++ client-ihms-brython/players.py
@@ -15,7 +15,7 @@ import mapping
 DEFAULT_ELO = 1500
 
 
-OPTIONS = ['inscrits', 'joueurs', 'arbitres', 'oisifs', 'remplaçants', 'classement', 'modérateurs', 'courriels non confirmés']
+OPTIONS = ['classement', 'inscrits', 'joueurs', 'arbitres', 'oisifs', 'remplaçants', 'modérateurs', 'courriels non confirmés']
 
 
 def get_detailed_rating(classic, role_id):
@@ -76,6 +76,257 @@ def get_global_rating(classic):
     ajax.get(url, blocking=True, headers={'content-type': 'application/json'}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
 
     return list(rating_list)
+
+
+def show_rating(classic, role_id):
+    """ show_rating """
+
+    def make_ratings_table(classic, role_id):
+
+        if role_id:
+
+            # for a given role
+            rating_list = get_detailed_rating(classic, role_id)
+
+        else:
+
+            # for all roles
+            detailed_rating_list = get_global_rating(classic)
+
+            # should be 7
+            number_roles = len({r[1] for r in detailed_rating_list})
+
+            # need to sum up per player
+            rating_list_dict = {}
+            for (classic2, role_id2, player_id, elo, change, game_id, number_games) in detailed_rating_list:
+
+                # avoid using loop variable
+                classic_found = classic2
+
+                # create entry if necessary
+                if player_id not in rating_list_dict:
+                    rating_list_dict[player_id] = {}
+                    rating_list_dict[player_id]['elo_sum'] = 0
+                    rating_list_dict[player_id]['number_games'] = 0
+                    rating_list_dict[player_id]['number_rated'] = 0
+
+                # update entry
+
+                # suming up
+                rating_list_dict[player_id]['elo_sum'] += elo
+                rating_list_dict[player_id]['number_games'] += number_games
+                rating_list_dict[player_id]['number_rated'] += 1
+
+                # last
+                rating_list_dict[player_id]['last_change'] = change
+                rating_list_dict[player_id]['last_game'] = game_id
+                rating_list_dict[player_id]['last_role'] = role_id2
+
+            rating_list = [[classic_found, v['last_role'], k, round((v['elo_sum'] + (number_roles - v['number_rated']) * DEFAULT_ELO) / number_roles), v['last_change'], v['last_game'], v['number_games']] for k, v in rating_list_dict.items()]
+
+        ratings_table = html.TABLE()
+
+        # the display order
+        fields = ['rank', 'player', 'elo', 'change', 'role', 'game', 'number']
+
+        # header
+        thead = html.THEAD()
+        for field in fields:
+            field_fr = {'rank': 'rang', 'player': 'joueur', 'elo': 'elo', 'change': 'dernier changement', 'role': 'avec le rôle', 'game': 'dans la partie', 'number': 'nombre de parties'}[field]
+            col = html.TD(field_fr)
+            thead <= col
+        ratings_table <= thead
+
+        row = html.TR()
+        for field in fields:
+            buttons = html.DIV()
+            if field in ['player', 'elo', 'change', 'role', 'game', 'number']:
+
+                # button for sorting
+                button = html.BUTTON("<>")
+                button.bind("click", lambda e, f=field: sort_by_callback(e, f))
+                buttons <= button
+
+            col = html.TD(buttons)
+            row <= col
+        ratings_table <= row
+
+        sort_by = storage['SORT_BY_RATINGS']
+        reverse_needed = bool(storage['REVERSE_NEEDED_RATINGS'] == 'True')
+
+        # 0 classic / 1 role_id / 2 player_id / 3 elo / 4 change / 5 game_id / 6 number games
+
+        if sort_by == 'player':
+            def key_function(r): return num2pseudo[r[2]].upper()  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+        elif sort_by == 'elo':
+            def key_function(r): return r[3]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+        elif sort_by == 'change':
+            def key_function(r): return r[4]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+        elif sort_by == 'role':
+            def key_function(r): return r[1]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+        elif sort_by == 'game':
+            def key_function(r): return games_dict[str(r[5])]['name'].upper()  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+        elif sort_by == 'number':
+            def key_function(r): return r[6]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
+
+        rank = 1
+
+        for rating in sorted(rating_list, key=key_function, reverse=reverse_needed):
+
+            player = num2pseudo[rating[2]]
+            if player == pseudo:
+                colour = config.MY_RATING
+            else:
+                colour = None
+
+            row = html.TR()
+            for field in fields:
+
+                if field == 'rank':
+                    value = rank
+
+                if field == 'player':
+                    value = player
+
+                if field == 'elo':
+                    value = rating[3]
+
+                if field == 'change':
+                    value = rating[4]
+
+                if field == 'role':
+                    role_id = rating[1]
+                    role = variant_data.roles[role_id]
+                    role_name = variant_data.name_table[role]
+                    role_icon_img = html.IMG(src=f"./variants/{variant_name}/{interface_chosen}/roles/{role_id}.jpg", title=role_name)
+                    value = role_icon_img
+
+                if field == 'game':
+                    value = games_dict[str(rating[5])]['name']
+
+                if field == 'number':
+                    value = rating[6]
+
+                col = html.TD(value)
+                if colour is not None:
+                    col.style = {
+                        'background-color': colour
+                    }
+
+                row <= col
+
+            ratings_table <= row
+            rank += 1
+
+        average = round(sum(r[3] for r in rating_list) / len(rating_list))
+        return ratings_table, average
+
+    def refresh():
+
+        ratings_table, average = make_ratings_table(classic, role_id)
+
+        # button for changing mode
+        switch_mode_button = html.BUTTON(f"passer en {'blitz' if classic else 'classique'}")
+        switch_mode_button.bind("click", switch_mode_callback)
+
+        # button for going global
+        switch_global_button = html.BUTTON("classement global")
+        switch_global_button.bind("click", lambda e: switch_role_callback(e, None))
+
+        # buttons for selecting role
+        switch_role_buttons_table = html.TABLE()
+        row = html.TR()
+        col = html.TD("Cliquer sur le pays pour le détail ->")
+        row <= col
+        for poss_role_id in variant_data.roles:
+            if poss_role_id >= 1 and poss_role_id != role_id:
+                form = html.FORM()
+                input_change_role = html.INPUT(type="image", src=f"./variants/{variant_name}/{interface_chosen}/roles/{poss_role_id}.jpg")
+                input_change_role.bind("click", lambda e, r=poss_role_id: switch_role_callback(e, r))
+                form <= input_change_role
+                col = html.TD(form)
+                row <= col
+        switch_role_buttons_table <= row
+
+        MY_SUB_PANEL.clear()
+        MY_SUB_PANEL <= html.H3(f"Le classement par ELO en mode {'classique' if classic else 'blitz'}")
+        MY_SUB_PANEL <= switch_mode_button
+        MY_SUB_PANEL <= html.BR()
+        MY_SUB_PANEL <= html.BR()
+        if role_id:
+            MY_SUB_PANEL <= switch_global_button
+            MY_SUB_PANEL <= html.BR()
+            MY_SUB_PANEL <= html.BR()
+        MY_SUB_PANEL <= switch_role_buttons_table
+        MY_SUB_PANEL <= html.BR()
+        MY_SUB_PANEL <= ratings_table
+        MY_SUB_PANEL <= html.BR()
+        MY_SUB_PANEL <= html.DIV(f"La moyenne des ELO est de {average}", Class='note')
+
+    def sort_by_callback(_, new_sort_by):
+
+        # if same sort criterion : inverse order otherwise back to normal order
+        if new_sort_by is not None:
+            if new_sort_by != storage['SORT_BY_RATINGS']:
+                storage['SORT_BY_RATINGS'] = new_sort_by
+                storage['REVERSE_NEEDED_RATINGS'] = str(False)
+            else:
+                storage['REVERSE_NEEDED_RATINGS'] = str(not bool(storage['REVERSE_NEEDED_RATINGS'] == 'True'))
+
+        refresh()
+
+    def switch_mode_callback(_):
+
+        nonlocal classic
+        classic = not classic
+        refresh()
+
+    def switch_role_callback(_, new_role_id):
+
+        nonlocal role_id
+        role_id = new_role_id
+        refresh()
+
+    if 'PSEUDO' in storage:
+        pseudo = storage['PSEUDO']
+    else:
+        pseudo = None
+
+    if 'GAME' not in storage:
+        alert("Il faut choisir la partie au préalable (pour la variante)")
+        return
+
+    if 'GAME_VARIANT' not in storage:
+        alert("Pas de partie de référence")
+        return
+    variant_name = storage['GAME_VARIANT']
+
+    variant_content = common.game_variant_content_reload(variant_name)
+    interface_chosen = interface.get_interface_from_variant(variant_name)
+    interface_parameters = common.read_parameters(variant_name, interface_chosen)
+
+    variant_data = mapping.Variant(variant_name, variant_content, interface_parameters)
+
+    players_dict = common.get_players()
+    if not players_dict:
+        alert("Erreur chargement dictionnaire joueurs")
+        return
+
+    # pseudo from number
+    num2pseudo = {v: k for k, v in players_dict.items()}
+
+    games_dict = common.get_games_data()
+    if not games_dict:
+        alert("Erreur chargement dictionnaire parties")
+        return
+
+    # default
+    if 'SORT_BY_RATINGS' not in storage:
+        storage['SORT_BY_RATINGS'] = 'elo'
+    if 'REVERSE_NEEDED_RATINGS' not in storage:
+        storage['REVERSE_NEEDED_RATINGS'] = str(True)
+
+    sort_by_callback(None, None)
 
 
 def show_registered_data():
@@ -387,257 +638,6 @@ def show_replacement_data():
     MY_SUB_PANEL <= html.H3("Les remplaçants")
     MY_SUB_PANEL <= players_table
     MY_SUB_PANEL <= html.P(f"Il y a {count} remplaçants")
-
-
-def show_rating(classic, role_id):
-    """ show_rating """
-
-    def make_ratings_table(classic, role_id):
-
-        if role_id:
-
-            # for a given role
-            rating_list = get_detailed_rating(classic, role_id)
-
-        else:
-
-            # for all roles
-            detailed_rating_list = get_global_rating(classic)
-
-            # should be 7
-            number_roles = len({r[1] for r in detailed_rating_list})
-
-            # need to sum up per player
-            rating_list_dict = {}
-            for (classic2, role_id2, player_id, elo, change, game_id, number_games) in detailed_rating_list:
-
-                # avoid using loop variable
-                classic_found = classic2
-
-                # create entry if necessary
-                if player_id not in rating_list_dict:
-                    rating_list_dict[player_id] = {}
-                    rating_list_dict[player_id]['elo_sum'] = 0
-                    rating_list_dict[player_id]['number_games'] = 0
-                    rating_list_dict[player_id]['number_rated'] = 0
-
-                # update entry
-
-                # suming up
-                rating_list_dict[player_id]['elo_sum'] += elo
-                rating_list_dict[player_id]['number_games'] += number_games
-                rating_list_dict[player_id]['number_rated'] += 1
-
-                # last
-                rating_list_dict[player_id]['last_change'] = change
-                rating_list_dict[player_id]['last_game'] = game_id
-                rating_list_dict[player_id]['last_role'] = role_id2
-
-            rating_list = [[classic_found, v['last_role'], k, round((v['elo_sum'] + (number_roles - v['number_rated']) * DEFAULT_ELO) / number_roles), v['last_change'], v['last_game'], v['number_games']] for k, v in rating_list_dict.items()]
-
-        ratings_table = html.TABLE()
-
-        # the display order
-        fields = ['rank', 'player', 'elo', 'change', 'role', 'game', 'number']
-
-        # header
-        thead = html.THEAD()
-        for field in fields:
-            field_fr = {'rank': 'rang', 'player': 'joueur', 'elo': 'elo', 'change': 'dernier changement', 'role': 'avec le rôle', 'game': 'dans la partie', 'number': 'nombre de parties'}[field]
-            col = html.TD(field_fr)
-            thead <= col
-        ratings_table <= thead
-
-        row = html.TR()
-        for field in fields:
-            buttons = html.DIV()
-            if field in ['player', 'elo', 'change', 'role', 'game', 'number']:
-
-                # button for sorting
-                button = html.BUTTON("<>")
-                button.bind("click", lambda e, f=field: sort_by_callback(e, f))
-                buttons <= button
-
-            col = html.TD(buttons)
-            row <= col
-        ratings_table <= row
-
-        sort_by = storage['SORT_BY_RATINGS']
-        reverse_needed = bool(storage['REVERSE_NEEDED_RATINGS'] == 'True')
-
-        # 0 classic / 1 role_id / 2 player_id / 3 elo / 4 change / 5 game_id / 6 number games
-
-        if sort_by == 'player':
-            def key_function(r): return num2pseudo[r[2]].upper()  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-        elif sort_by == 'elo':
-            def key_function(r): return r[3]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-        elif sort_by == 'change':
-            def key_function(r): return r[4]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-        elif sort_by == 'role':
-            def key_function(r): return r[1]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-        elif sort_by == 'game':
-            def key_function(r): return games_dict[str(r[5])]['name'].upper()  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-        elif sort_by == 'number':
-            def key_function(r): return r[6]  # noqa: E704 # pylint: disable=multiple-statements, invalid-name
-
-        rank = 1
-
-        for rating in sorted(rating_list, key=key_function, reverse=reverse_needed):
-
-            player = num2pseudo[rating[2]]
-            if player == pseudo:
-                colour = config.MY_RATING
-            else:
-                colour = None
-
-            row = html.TR()
-            for field in fields:
-
-                if field == 'rank':
-                    value = rank
-
-                if field == 'player':
-                    value = player
-
-                if field == 'elo':
-                    value = rating[3]
-
-                if field == 'change':
-                    value = rating[4]
-
-                if field == 'role':
-                    role_id = rating[1]
-                    role = variant_data.roles[role_id]
-                    role_name = variant_data.name_table[role]
-                    role_icon_img = html.IMG(src=f"./variants/{variant_name}/{interface_chosen}/roles/{role_id}.jpg", title=role_name)
-                    value = role_icon_img
-
-                if field == 'game':
-                    value = games_dict[str(rating[5])]['name']
-
-                if field == 'number':
-                    value = rating[6]
-
-                col = html.TD(value)
-                if colour is not None:
-                    col.style = {
-                        'background-color': colour
-                    }
-
-                row <= col
-
-            ratings_table <= row
-            rank += 1
-
-        average = round(sum(r[3] for r in rating_list) / len(rating_list))
-        return ratings_table, average
-
-    def refresh():
-
-        ratings_table, average = make_ratings_table(classic, role_id)
-
-        # button for changing mode
-        switch_mode_button = html.BUTTON(f"passer en {'blitz' if classic else 'classique'}")
-        switch_mode_button.bind("click", switch_mode_callback)
-
-        # button for going global
-        switch_global_button = html.BUTTON("classement global")
-        switch_global_button.bind("click", lambda e: switch_role_callback(e, None))
-
-        # buttons for selecting role
-        switch_role_buttons_table = html.TABLE()
-        row = html.TR()
-        col = html.TD("Cliquer sur le pays pour le détail ->")
-        row <= col
-        for poss_role_id in variant_data.roles:
-            if poss_role_id >= 1 and poss_role_id != role_id:
-                form = html.FORM()
-                input_change_role = html.INPUT(type="image", src=f"./variants/{variant_name}/{interface_chosen}/roles/{poss_role_id}.jpg")
-                input_change_role.bind("click", lambda e, r=poss_role_id: switch_role_callback(e, r))
-                form <= input_change_role
-                col = html.TD(form)
-                row <= col
-        switch_role_buttons_table <= row
-
-        MY_SUB_PANEL.clear()
-        MY_SUB_PANEL <= html.H3(f"Le classement par ELO en mode {'classique' if classic else 'blitz'}")
-        MY_SUB_PANEL <= switch_mode_button
-        MY_SUB_PANEL <= html.BR()
-        MY_SUB_PANEL <= html.BR()
-        if role_id:
-            MY_SUB_PANEL <= switch_global_button
-            MY_SUB_PANEL <= html.BR()
-            MY_SUB_PANEL <= html.BR()
-        MY_SUB_PANEL <= switch_role_buttons_table
-        MY_SUB_PANEL <= html.BR()
-        MY_SUB_PANEL <= ratings_table
-        MY_SUB_PANEL <= html.BR()
-        MY_SUB_PANEL <= html.DIV(f"La moyenne des ELO est de {average}", Class='note')
-
-    def sort_by_callback(_, new_sort_by):
-
-        # if same sort criterion : inverse order otherwise back to normal order
-        if new_sort_by is not None:
-            if new_sort_by != storage['SORT_BY_RATINGS']:
-                storage['SORT_BY_RATINGS'] = new_sort_by
-                storage['REVERSE_NEEDED_RATINGS'] = str(False)
-            else:
-                storage['REVERSE_NEEDED_RATINGS'] = str(not bool(storage['REVERSE_NEEDED_RATINGS'] == 'True'))
-
-        refresh()
-
-    def switch_mode_callback(_):
-
-        nonlocal classic
-        classic = not classic
-        refresh()
-
-    def switch_role_callback(_, new_role_id):
-
-        nonlocal role_id
-        role_id = new_role_id
-        refresh()
-
-    if 'PSEUDO' in storage:
-        pseudo = storage['PSEUDO']
-    else:
-        pseudo = None
-
-    if 'GAME' not in storage:
-        alert("Il faut choisir la partie au préalable (pour la variante)")
-        return
-
-    if 'GAME_VARIANT' not in storage:
-        alert("Pas de partie de référence")
-        return
-    variant_name = storage['GAME_VARIANT']
-
-    variant_content = common.game_variant_content_reload(variant_name)
-    interface_chosen = interface.get_interface_from_variant(variant_name)
-    interface_parameters = common.read_parameters(variant_name, interface_chosen)
-
-    variant_data = mapping.Variant(variant_name, variant_content, interface_parameters)
-
-    players_dict = common.get_players()
-    if not players_dict:
-        alert("Erreur chargement dictionnaire joueurs")
-        return
-
-    # pseudo from number
-    num2pseudo = {v: k for k, v in players_dict.items()}
-
-    games_dict = common.get_games_data()
-    if not games_dict:
-        alert("Erreur chargement dictionnaire parties")
-        return
-
-    # default
-    if 'SORT_BY_RATINGS' not in storage:
-        storage['SORT_BY_RATINGS'] = 'elo'
-    if 'REVERSE_NEEDED_RATINGS' not in storage:
-        storage['REVERSE_NEEDED_RATINGS'] = str(True)
-
-    sort_by_callback(None, None)
 
 
 def show_moderators():
