2a2f5467fe6895347e5081ba29ac4a09ce3cc45b
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2022-09-18 11:25:27 +0200

    .

--- rest-servers/games/server.py
+++ rest-servers/games/server.py
@@ -990,6 +990,10 @@ class AllocationListRessource(flask_restful.Resource):  # type: ignore
             flask_restful.abort(404, msg=f"Failed to get id from player_pseudo {message}")
         player_id = req_result.json()
 
+        # abort special case : we do not want to see this player in more games
+        if not delete and player_pseudo == "Chryss":
+            flask_restful.abort(404, msg="Core dumped, segmentation fault!")
+
         sql_executor = database.SqlExecutor()
 
         # find the game
