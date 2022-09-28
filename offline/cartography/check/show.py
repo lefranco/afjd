41b51619c3a056028e71e995ef2444f2595a2e88
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2022-09-28 09:23:58 +0200

    .

---
+++ offline/cartography/check/show.py
@@ -0,0 +1,100 @@
+""" submit """
+
+# pylint: disable=pointless-statement, expression-not-assigned
+
+import json
+import datetime
+import enum
+import time
+
+from browser import document, html, ajax, alert   # pylint: disable=import-error
+from browser.widgets.dialog import InfoDialog  # pylint: disable=import-error
+from browser.local_storage import storage  # pylint: disable=import-error
+
+MAP_WIDTH=667
+MAP_HEIGHT=673
+MAP_FILE='map.png'
+
+my_panel = html.DIV(id="play")
+my_panel.attrs['style'] = 'display: table-row'
+
+my_sub_panel = html.DIV(id="sub")
+my_panel <= my_sub_panel
+
+
+class PositionRecord:
+    """ A position """
+
+    def __init__(self, x_pos, y_pos) -> None:
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+    def __str__(self) :
+        return f"x={self.x_pos} y={self.y_pos}"
+
+
+def show_position():
+    """ show_position """
+
+    hovering_message = "(informations par la souris sur la carte)"
+
+
+    def callback_render(_):
+        """ callback_render """
+
+        # put the background map first
+        ctx.drawImage(img, 0, 0)
+
+        # put the legends
+        #variant_data.render(ctx)
+
+        # put the position
+        #position_data.render(ctx)
+
+    def callback_canvas_mouse_move(event):
+        """ callback_canvas_mouse_move """
+
+        pos = PositionRecord(x_pos=event.x - canvas.abs_left, y_pos=event.y - canvas.abs_top)
+        hover_info.text = str(pos)
+
+        # TODO
+
+    def callback_canvas_mouse_leave(_):
+        """ callback_canvas_mouse_leave """
+        hover_info.text = hovering_message
+
+    # now we can display
+
+    # create canvas
+    canvas = html.CANVAS(id="map_canvas", width=MAP_WIDTH, height=MAP_HEIGHT, alt="Map of the game")
+    ctx = canvas.getContext("2d")
+    if ctx is None:
+        alert("Il faudrait utiliser un navigateur plus récent !")
+        return
+
+    # probably useless
+    ctx.imageSmoothingEnabled = False;
+
+    # give some information sometimes
+    canvas.bind("mousemove", callback_canvas_mouse_move)
+    canvas.bind("mouseleave", callback_canvas_mouse_leave)
+
+    # put background (this will call the callback that display the whole map)
+    img = html.IMG(src=MAP_FILE)
+    img.bind('load', callback_render)
+
+    hover_info = html.DIV(hovering_message)
+    hover_info.style = {
+        'color': 'blue',
+    }
+
+    my_sub_panel <= hover_info
+    my_sub_panel <= canvas
+
+
+
+def render(panel_middle):
+    """ render """
+
+    show_position()
+    panel_middle <= my_panel
---
+++ offline/cartography/optimize.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+
+"""
+File : optimize.py
+
+optimize parameter file
+"""
+
+import argparse
+import json
+import itertools
+import math
+import sys
+
+SCALING_X = 5.
+SCALING_Y = 5.
+MAX_DIST = 25
+
+class Point:
+    """ Point """
+
+    def __init__(self, x, y) -> None:
+        self.x = x  # pylint: disable=invalid-name
+        self.y = y  # pylint: disable=invalid-name
+
+    def distance(self, other) -> float:
+        """ distance """
+        return math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
+
+    def __str__(self) -> str:
+        return f"x={self.x} y={self.y}"
+
+
+def main() -> None:
+    """ main """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--parameters_input', required=True, help='Input parameters json file')
+    parser.add_argument('-F', '--first_format_json_output', required=True, help='Output optimized parameters json file')
+    args = parser.parse_args()
+
+    json_parameters_input = args.parameters_input
+    first_format_json_output = args.first_format_json_output
+
+    # load parameters from json data file
+    with open(json_parameters_input, "r", encoding='utf-8') as read_file:
+        json_parameters_data = json.load(read_file)
+
+    # ============= optim ===============
+
+    items: typing.Dict[Point, typing.Any] = {}
+
+    for zone in json_parameters_data['zones'].values():
+
+        # ignore specific coasts
+        if not zone['name']:
+            continue
+
+        x_pos = zone['x_pos']
+        y_pos = zone['y_pos']
+        point = Point(x_pos, y_pos)
+        items[point] = zone
+
+    iterations = 1
+    while True:
+
+        smallest_dist = sys.float_info.max
+        for point1, point2 in itertools.combinations(items.keys(), 2):
+            dist = point1.distance(point2)
+            if dist < smallest_dist:
+                couple = (point1, point2)
+                smallest_dist = dist
+
+        if smallest_dist > MAX_DIST:
+            break
+
+        (point1, point2) = couple
+        zone1 = items[point1]
+        zone2 = items[point2]
+        print(f"Pushing aside {zone1['name']} and {zone2['name']} distant of {smallest_dist}")
+
+        # push them aside
+        point1.x += round((point1.x - point2.x) / SCALING_X)
+        point1.y += round((point1.y - point2.y) / SCALING_Y)
+        point2.x += round((point2.x - point1.x) / SCALING_X)
+        point2.y += round((point2.y - point1.y) / SCALING_Y)
+
+
+    # ============= output ===============
+
+    for point, element in items.items():
+
+        delta_x = element['x_legend_pos'] - element['x_pos']
+        element['x_pos'] = point.x
+        element['x_legend_pos'] = element['x_pos'] + delta_x
+
+        delta_y = element['y_legend_pos'] - element['y_pos']
+        element['y_pos'] = point.y
+        element['y_legend_pos'] = element['y_pos'] + delta_y
+
+    output = json.dumps(json_parameters_data, indent=4, ensure_ascii=False)
+    with open(first_format_json_output, 'w', encoding='utf-8') as file_ptr:
+        file_ptr.write(output)
+
+
+if __name__ == '__main__':
+    main()
