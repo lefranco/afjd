1c303822a485e173a4f2a1037b6c23c57804b6c9
Jérémie LEFRANCOIS <jlefrancois-ext@gaci.fr>
2022-10-03 16:18:55 +0200

    .

--- client-ihms-brython/events.py
+++ client-ihms-brython/events.py
@@ -758,8 +758,8 @@ def edit_event():
 def illustrate_event():
     """ illustrate_event """
 
-    def illustrate_event_callback(_):
-        """ illustrate_event_callback """
+    def put_picture_event_callback(_):
+        """ put_picture_event_callback """
 
         def onload_callback(_):
             """ onload_callback """
@@ -778,11 +778,15 @@ def illustrate_event():
                 messages = "<br>".join(req_result['msg'].split('\n'))
                 InfoDialog("OK", f"L'événement a été illustré : {messages}", remove_after=config.REMOVE_AFTER)
 
-            # get the imùage content
+            # get the image content
             image_bytes = bytes(window.Array["from"](window.Uint8Array.new(reader.result)))
 
             # b64 encode to pass it on server
-            image_str = base64.standard_b64encode(image_bytes).decode()
+            try:
+                image_str = base64.standard_b64encode(image_bytes).decode()
+            except:
+                alert("Problème à l'encodage pour le web... fichier trop gros ?")
+                return
 
             json_dict = {
                 'image': image_str
@@ -796,6 +800,7 @@ def illustrate_event():
             ajax.put(url, blocking=True, headers={'content-type': 'application/json', 'AccessToken': storage['JWT_TOKEN']}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
 
         if not INPUT_FILE.files:
+
             alert("Pas de fichier")
 
             # back to where we started
@@ -810,6 +815,38 @@ def illustrate_event():
         # Read the file content as text
         reader.bind("load", onload_callback)
         reader.readAsArrayBuffer(file_name)
+
+        # back to where we started
+        MY_SUB_PANEL.clear()
+        illustrate_event()
+
+    def remove_picture_event_callback(_):
+        """ remove_picture_event_callback """
+
+        def reply_callback(req):
+            req_result = json.loads(req.text)
+            if req.status != 200:
+                if 'message' in req_result:
+                    alert(f"Erreur à la non illustration de l'événement : {req_result['message']}")
+                elif 'msg' in req_result:
+                    alert(f"Problème à la non illustration de l'événement : {req_result['msg']}")
+                else:
+                    alert("Réponse du serveur imprévue et non documentée")
+                return
+
+            messages = "<br>".join(req_result['msg'].split('\n'))
+            InfoDialog("OK", f"L'événement a été non illustré : {messages}", remove_after=config.REMOVE_AFTER)
+
+        json_dict = {
+            'image': ''
+        }
+
+        host = config.SERVER_CONFIG['PLAYER']['HOST']
+        port = config.SERVER_CONFIG['PLAYER']['PORT']
+        url = f"{host}:{port}/events/{event_id}"
+
+        # illustrating an event : need token
+        ajax.put(url, blocking=True, headers={'content-type': 'application/json', 'AccessToken': storage['JWT_TOKEN']}, timeout=config.TIMEOUT_SERVER, data=json.dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
 
         # back to where we started
         MY_SUB_PANEL.clear()
@@ -859,9 +896,13 @@ def illustrate_event():
 
     form <= html.BR()
 
-    input_create_games = html.INPUT(type="submit", value="mettre cette image")
-    input_create_games.bind("click", illustrate_event_callback)
-    form <= input_create_games
+    input_put_picture = html.INPUT(type="submit", value="mettre cette image")
+    input_put_picture.bind("click", put_picture_event_callback)
+    form <= input_put_picture
+
+    input_remove_picture = html.INPUT(type="submit", value="retirer l'image")
+    input_remove_picture.bind("click", remove_picture_event_callback)
+    form <= input_remove_picture
 
     MY_SUB_PANEL <= html.DIV(f"Evénement {event_name}", Class='important')
     MY_SUB_PANEL <= html.BR()
