38d8a641ac4c09d100e0b0bff0814ef01e56feba
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2022-10-16 18:18:30 +0200

    .

--- offline/cartography/invoke.sh
+++ offline/cartography/invoke.sh
@@ -2,9 +2,9 @@
 
 echo "Parsing...."
 ./parse.py \
-   -m ../client-ihms-brython/variants/standard/diplomania/map.png \
-   -v ../rest-servers/games/variants/standard.json \
-   -p ../client-ihms-brython/variants/standard/diplomania/parameters.json \
+   -m ../../client-ihms-brython/variants/standard/diplomania/map.png \
+   -v ../../rest-servers/games/variants/standard.json \
+   -p ../../client-ihms-brython/variants/standard/diplomania/parameters.json \
    -s ./input/Carte_annotee.svg \
    -F new_parameters.json
 
@@ -12,6 +12,3 @@ echo "Optimizing...."
 ./optimize.py \
    -p new_parameters.json \
    -F new_parameters_opt.json
-
-
-exit
