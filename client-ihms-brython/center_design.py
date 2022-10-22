6b761ebea779bfbf08bcbc8a030c6c507d8956d3
Jeremie Lefrancois <jeremie.lefrancois@gmail.com>
2022-10-22 17:53:48 +0200

    repaired sick unit bug

--- client-ihms-brython/center_design.py
+++ client-ihms-brython/center_design.py
@@ -11,6 +11,8 @@ CENTER_RAY = 5
 def stabbeur_center(x: int, y: int, ctx):  # pylint: disable=invalid-name
     """ display a center the stabbeur way """
 
+    # the ctx.strokeStyle and ctx.fillStyle should be defined
+
     ctx.beginPath()
     ctx.arc(x, y, CENTER_RAY, 0, 2 * math.pi, False)
     ctx.fill(); ctx.stroke(); ctx.closePath()
