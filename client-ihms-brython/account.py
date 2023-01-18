ce3790fe33c680f654d104551dfc339e21c47666
lefranco <jeremie.lefrancois@gmail.com>
2023-01-18 08:49:02 +0100

    .

--- client-ihms-brython/account.py
+++ client-ihms-brython/account.py
@@ -45,16 +45,16 @@ def information_about_account():
 def information_about_account2():
     """ information_about_account2 """
 
-    information = html.DIV(Class='note')
-    information <= f"A NOTER : Un compte oisif plus de {config.IDLE_DAY_TIMEOUT} jours sera supprimé pour ne pas encombrer le système."
+    information = html.DIV(Class='important')
+    information <= f"ATTENTION : Un compte oisif plus de {config.IDLE_DAY_TIMEOUT} jours sera supprimé pour ne pas encombrer le système."
     return information
 
 
 def information_about_emails():
     """ information_about_emails """
 
-    information = html.DIV(Class='important')
-    information <= "Vous recevrez un courriel au démarrage et à l'arrêt de vos parties. Parfois un courriel de rappel de l'arbitre si vous êtes en retard sur les ordres. Un courriel de notification de résolution et/ou de message ou presse dans la partie mais dans ce dernier cas seulement si vous l'avez demandé. Un courriel à la suppression du compte. Un courriel à chaque modification d'adresse courriel pour vérifier celle-ci"
+    information = html.DIV(Class='note')
+    information <= "Vous recevrez un courriel au démarrage et à l'arrêt de vos parties. Un courriel de rappel de l'arbitre si vous êtes en retard sur les ordres. Un courriel de notification de résolution et/ou de message ou presse dans la partie mais dans ce dernier cas seulement si vous l'avez demandé. Un courriel à la suppression du compte. Un courriel à chaque modification d'adresse courriel pour vérifier celle-ci..."
     information <= html.BR()
     information <= "Rien de plus !"
     information <= html.BR()
