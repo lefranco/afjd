dc90831bc7f3287671e7363599456e88c93b9200
lefranco <jeremie.lefrancois@gmail.com>
2023-03-04 06:15:28 +0100

    .

--- client-ihms-brython/admin.py
+++ client-ihms-brython/admin.py
@@ -2228,7 +2228,7 @@ def agreement_usage():
                         colour = config.PASSED_DEADLINE_COLOUR
                 else:
                     # we are after everything !
-                    if time_stamp_now > deadline_loaded + 60 * 60 * 24 * 7:
+                    if time_stamp_now > deadline_loaded + 60 * 60 * 24 * config.CRITICAL_DELAY_DAY:
                         colour = config.CRITICAL_COLOUR
                     # we are after deadline + grace
                     elif time_stamp_now > deadline_loaded + 60 * 60 * data['grace_duration']:
--- client-ihms-brython/moderate.py
+++ client-ihms-brython/moderate.py
@@ -1069,7 +1069,7 @@ def show_player_games(pseudo_player, game_list):
                             colour = config.PASSED_DEADLINE_COLOUR
                     else:
                         # we are after everything !
-                        if time_stamp_now > deadline_loaded + 60 * 60 * 24 * 7:
+                        if time_stamp_now > deadline_loaded + 60 * 60 * 24 * config.CRITICAL_DELAY_DAY:
                             colour = config.CRITICAL_COLOUR
                         # we are after deadline + grace
                         elif time_stamp_now > deadline_loaded + 60 * 60 * data['grace_duration']:
