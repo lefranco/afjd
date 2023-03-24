6cd41131f23bc33d42e11f17004d3653a20c351d
lefranco <jeremie.lefrancois@gmail.com>
2023-03-24 09:02:31 +0100

    moderator can remove newsletter for account

--- rest-servers/players/server.py
+++ rest-servers/players/server.py
@@ -1194,6 +1194,63 @@ class PlayerEmailRessource(flask_restful.Resource):  # type: ignore
         return data, 200
 
 
+@API.resource('/remove-newsletter/<pseudo_player>')
+class RemoveNewsletterRessource(flask_restful.Resource):  # type: ignore
+    """ RemoveNewsletterRessource """
+
+    def post(self, pseudo_player: str) -> typing.Tuple[typing.Dict[str, str], int]:
+        """
+        Patch newsletter bit for a player (he complained !)
+        EXPOSED
+        """
+
+        mylogger.LOGGER.info("/remove-newsletter - POST - remove newsletter pseudo=%s", pseudo_player)
+
+        # check from user server user is pseudo
+        host = lowdata.SERVER_CONFIG['USER']['HOST']
+        port = lowdata.SERVER_CONFIG['USER']['PORT']
+        url = f"{host}:{port}/verify"
+        jwt_token = flask.request.headers.get('AccessToken')
+        if not jwt_token:
+            flask_restful.abort(400, msg="Missing authentication!")
+        req_result = SESSION.get(url, headers={'Authorization': f"Bearer {jwt_token}"})
+        if req_result.status_code != 200:
+            mylogger.LOGGER.error("ERROR = %s", req_result.text)
+            message = req_result.json()['msg'] if 'msg' in req_result.json() else "???"
+            flask_restful.abort(400, msg=f"Bad authentication!:{message}")
+
+        pseudo = req_result.json()['logged_in_as']
+
+        sql_executor = database.SqlExecutor()
+
+        requester = players.Player.find_by_pseudo(sql_executor, pseudo)
+
+        if requester is None:
+            del sql_executor
+            flask_restful.abort(404, msg=f"Requesting player {pseudo} does not exist")
+
+        moderators_list = moderators.Moderator.inventory(sql_executor)
+        the_moderators = [m[0] for m in moderators_list]
+        if pseudo not in the_moderators:
+            del sql_executor
+            flask_restful.abort(403, msg="You are not allowed to changed newsletters of someone! (need to be moderator)")
+
+        wild = players.Player.find_by_pseudo(sql_executor, pseudo_player)
+
+        if wild is None:
+            del sql_executor
+            flask_restful.abort(404, msg=f"Wild player {pseudo_player} does not exist")
+
+        assert wild is not None
+        wild.newsletter = False
+        wild.update_database(sql_executor)
+        sql_executor.commit()
+        del sql_executor
+
+        data = {'msg': 'Ok wild patched'}
+        return data, 200
+
+
 @API.resource('/creators')
 class CreatorListRessource(flask_restful.Resource):  # type: ignore
     """ CreatorListRessource """
