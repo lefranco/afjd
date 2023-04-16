4f9041c8c88d144cbfa10480c4b52c898fb50b08
lefranco <jeremie.lefrancois@gmail.com>
2023-04-16 09:44:21 +0200

    .

--- client-ihms-brython/tips.py
+++ client-ihms-brython/tips.py
@@ -3,21 +3,21 @@
 
 TIPS_CONTENT_TABLE = {
 
-    "Pour avoir les parties dans des onglets séparés sur votre smartphone :":
+    "Pour avoir les parties dans des onglets séparés sur votre smartphone":
     "Utiliser le menu “Mes parties” bouton “basculer en mode liens externes”",
 
     "Pour être contacté en cas de besoin de remplaçant : ":
     "Utiliser le menu “Mon compte” sous-menu “Editer mon compte” et modifiez le paramètre approprié",
 
-    "Pour entrer des 'faux' ordres (parties sans communication possible) :":
+    "Pour entrer des 'faux' ordres (parties sans communication possible)":
     "Utiliser le sous menu “Taguer” depuis la page de la partie",
 
-    "Pour se désinscrire d'une partie (inscrit par erreur) :":
+    "Pour se désinscrire d'une partie (inscrit par erreur)":
     "Utiliser le menu “Rejoindre un partie” sous-menu “Sélectionner partie” puis option “Appariement” et enfin bouton quitter la partie",
 
-    "Pour les daltoniens : ":
+    "Pour les daltoniens":
     "Une carte avec des couleurs spécifiques a été créée, menu “Technique” option “Choix d'interface”",
 
-    "Pour les possesseurs d'appareil Android ":
+    "Pour les possesseurs d'appareil Android":
     "Une application spécifique a été créée, allez sur le forum pour la télécharger",
 }
