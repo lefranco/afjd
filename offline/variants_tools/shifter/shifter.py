50c6be584f56e719439af5d0ef91ff275d5cac7f
lefranco <jeremie.lefrancois@gmail.com>
2023-08-29 22:05:47 +0200

    .

--- offline/variants_tools/shifter/shifter.py
+++ offline/variants_tools/shifter/shifter.py
@@ -15,9 +15,13 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_file', required=True, help='Input  json file')
+    parser.add_argument('-x', '--x_shift', required=True, help='x_shift')
+    parser.add_argument('-y', '--y_shift', required=True, help='y_shift')
     args = parser.parse_args()
 
     json_input = args.input_file
+    x_shift = args.x_shift
+    y_shift = args.y_shift
     json_output = f"{json_input}.shifted"
 
     # load parameters from json data file
@@ -30,8 +34,10 @@ def main() -> None:
 
     for zone_data in json_data['zones'].values():
 
-        zone_data["y_pos"] += 6
-        zone_data["y_legend_pos"] += 6
+        zone_data["x_pos"] += x_shift
+        zone_data["x_legend_pos"] += x_shift
+        zone_data["y_pos"] += y_shift
+        zone_data["y_legend_pos"] += y_shift
 
     output = json.dumps(json_data, indent=4, ensure_ascii=False)
     with open(json_output, 'w', encoding='utf-8') as file_ptr:
