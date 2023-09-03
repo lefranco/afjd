012889b3df9f49fd17a780a8adc5e22f93f160ec
lefranco <jeremie.lefrancois@gmail.com>
2023-09-03 17:27:03 +0200

    now shifts everything

--- offline/variants_tools/shifter/shifter.py
+++ offline/variants_tools/shifter/shifter.py
@@ -15,8 +15,8 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_file', required=True, help='Input  json file')
-    parser.add_argument('-x', '--x_shift', required=True, help='x_shift')
-    parser.add_argument('-y', '--y_shift', required=True, help='y_shift')
+    parser.add_argument('-x', '--x_shift', required=True, type=int, help='x_shift')
+    parser.add_argument('-y', '--y_shift', required=True, type=int, help='y_shift')
     args = parser.parse_args()
 
     json_input = args.input_file
@@ -39,9 +39,21 @@ def main() -> None:
         zone_data["y_pos"] += y_shift
         zone_data["y_legend_pos"] += y_shift
 
+    for center_data in json_data['centers'].values():
+
+        center_data["x_pos"] += x_shift
+        center_data["y_pos"] += y_shift
+
+    for zone_areas_data in json_data['zone_areas'].values():
+        zone_areas_data['area'] = [[x+x_shift, y+y_shift] for x,y in zone_areas_data['area']]
+
+
+
     output = json.dumps(json_data, indent=4, ensure_ascii=False)
     with open(json_output, 'w', encoding='utf-8') as file_ptr:
         file_ptr.write(output)
+
+    print(f"Modified file copied to {json_output}")
 
 
 if __name__ == '__main__':
