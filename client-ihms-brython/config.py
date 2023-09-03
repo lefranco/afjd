1fa5053d455a4e5777fd13ab0455eafe1dec2ae3
lefranco <jeremie.lefrancois@gmail.com>
2023-09-03 13:54:39 +0200

    aegemonie (demo of map size only)

--- client-ihms-brython/config.py
+++ client-ihms-brython/config.py
@@ -36,7 +36,7 @@ with open("./data/scoring_list.json", "r", encoding="utf-8") as read_file:
     SCORING_CODE_TABLE = json.load(read_file)
 
 # default is first one
-VARIANT_NAMES_LIST = ['standard', 'grandeguerre', 'brouillard', 'hundred', 'moderne']
+VARIANT_NAMES_LIST = ['standard', 'grandeguerre', 'brouillard', 'hundred', 'moderne', 'aegemonie']
 
 
 # yellow before that
