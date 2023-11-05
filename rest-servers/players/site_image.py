ea22d0d8de85e2ed06fb870975832acba2350e89
lefranco <jeremie.lefrancois@gmail.com>
2023-11-05 09:24:45 +0100

    legend added to site image that admin may change

--- rest-servers/players/site_image.py
+++ rest-servers/players/site_image.py
@@ -17,21 +17,22 @@ class SiteImage:
     def content(sql_executor: database.SqlExecutor) -> bytes:
         """ get content """
 
-        content_found = sql_executor.execute("SELECT content FROM site_image", None, need_result=True)
-        import sys
-        print(f"{content_found=}", file=sys.stderr)
-        content = content_found[0][0]  # type: ignore
-        return content  # type: ignore
+        item_found = sql_executor.execute("SELECT * FROM site_image", None, need_result=True)
+        item = item_found[0]  # type: ignore
+        return item  # type: ignore
 
     @staticmethod
     def create_table(sql_executor: database.SqlExecutor) -> None:
         """ creation of table from scratch """
 
         sql_executor.execute("DROP TABLE IF EXISTS site_image")
-        sql_executor.execute("CREATE TABLE site_image (content BLOB)")
-        sql_executor.execute("INSERT INTO site_image (content) VALUES (?)", (" ",))
+        sql_executor.execute("CREATE TABLE site_image (legend STR, content BLOB)")
+        sql_executor.execute("INSERT INTO site_image (legend, content) VALUES (?, ?)", (" ", " "))
 
-    def __init__(self, content: bytes) -> None:
+    def __init__(self, legend: str, content: bytes) -> None:
+
+        assert isinstance(legend, str), "legend must be a str"
+        self._legend = legend
 
         assert isinstance(content, bytes), "content must be an bytes"
         self._content = content
@@ -39,14 +40,14 @@ class SiteImage:
     def update_database(self, sql_executor: database.SqlExecutor) -> None:
         """ Pushes changes from object to database """
         sql_executor.execute("DELETE FROM site_image")
-        sql_executor.execute("INSERT OR REPLACE INTO site_image (content) VALUES (?)", (self._content,))
+        sql_executor.execute("INSERT OR REPLACE INTO site_image (legend, content) VALUES (?, ?)", (self._legend, self._content))
 
     def delete_database(self, sql_executor: database.SqlExecutor) -> None:
         """ Removes object from database """
         sql_executor.execute("DELETE FROM site_image ")
 
     def __str__(self) -> str:
-        return f"image={self._content[0:5]!r}"
+        return f"legend={self._legend} image={self._content[0:5]!r}"
 
 
 if __name__ == '__main__':
