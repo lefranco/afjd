4c1ba311261d4f0becc19c763516bf3460649be2
lefranco <jeremie.lefrancois@gmail.com>
2023-11-11 16:29:57 +0100

    added a health checker

---
+++ health-check/check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "check users"
+curl https://users.diplomania.fr/login -H "Content-Type: application/json" -d '{"user_name":"one", "password":"one"}' -X  POST
+
+echo "check players"
+curl https://players.diplomania.fr/site_image -X GET | cut -d, -f 1
+
+echo "check games + its database"
+curl https://games.diplomania.fr/statistics -X GET
+
+echo "check emails"
+curl https://emails.diplomania.fr/send-email-simple -H "Content-Type: application/json" -d '{"subject":"test", "body":"test", "email":"jeremie.lefrancois@gmail.com"}' -X POST
+
+echo "check solver"
+curl https://solver.diplomania.fr/command -H "Content-Type: application/json" -d '{"command":"ls"}' -X POST
