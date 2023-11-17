d4d6832beed6f742f6beb079f651d66664caadcb
lefranco <jeremie.lefrancois@gmail.com>
2023-11-17 08:20:54 +0100

    .

--- offline/variants_tools/script/resizer/resizer.py
+++ offline/variants_tools/script/resizer/resizer.py
@@ -15,14 +15,12 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_file', required=True, help='Input  json file')
-    parser.add_argument('-x', '--x_shift', required=True, type=int, help='x_shift')
-    parser.add_argument('-y', '--y_shift', required=True, type=int, help='y_shift')
+    parser.add_argument('-s', '--scale', required=True, type=float, help='scale')
     args = parser.parse_args()
 
     json_input = args.input_file
-    x_shift = args.x_shift
-    y_shift = args.y_shift
-    json_output = f"{json_input}.shifted"
+    scale = args.scale
+    json_output = f"{json_input}.scaled"
 
     # load parameters from json data file
     with open(json_input, "r", encoding='utf-8') as read_file:
@@ -34,18 +32,18 @@ def main() -> None:
 
     for zone_data in json_data['zones'].values():
 
-        zone_data["x_pos"] += x_shift
-        zone_data["x_legend_pos"] += x_shift
-        zone_data["y_pos"] += y_shift
-        zone_data["y_legend_pos"] += y_shift
+        zone_data["x_pos"] = round(zone_data["x_pos"] * scale)
+        zone_data["x_legend_pos"]  = round(zone_data["x_legend_pos"] * scale)
+        zone_data["y_pos"] = round(zone_data["y_pos"] * scale)
+        zone_data["y_legend_pos"] = round(zone_data["y_legend_pos"] * scale)
 
     for center_data in json_data['centers'].values():
 
-        center_data["x_pos"] += x_shift
-        center_data["y_pos"] += y_shift
+        center_data["x_pos"] = round(center_data["x_pos"] * scale)
+        center_data["y_pos"] = round(center_data["y_pos"] * scale)
 
     for zone_areas_data in json_data['zone_areas'].values():
-        zone_areas_data['area'] = [[x+x_shift, y+y_shift] for x,y in zone_areas_data['area']]
+        zone_areas_data['area'] = [[round(x*scale), round(y*scale)] for x,y in zone_areas_data['area']]
 
     output = json.dumps(json_data, indent=4, ensure_ascii=False)
     with open(json_output, 'w', encoding='utf-8') as file_ptr:
