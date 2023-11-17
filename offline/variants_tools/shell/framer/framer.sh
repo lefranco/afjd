73e0dc49dedcc951fd22c495783204d0f1836a4f
lefranco <jeremie.lefrancois@gmail.com>
2023-11-17 08:20:54 +0100

    .

--- offline/variants_tools/shell/framer/framer.sh
+++ offline/variants_tools/shell/framer/framer.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+echo "Draws a frame around picture"
+
+if [ -z $1 ] ; then
+    echo "Missing argument 1 file name"
+    exit
+fi
+
+if [ ! -f $1 ] ; then
+    echo "File $1 does not exist"
+    exit
+fi
+
 w=$(convert $1 -format "%[w]" info:)
 h=$(convert $1 -format "%[h]" info:)
 cp $1 $1.framed
+
 # left
 convert $1.framed -strokewidth 1 -stroke black -draw "line 0,0 0,$(expr $h - 1)" $1.framed
 # right
--- offline/variants_tools/shell/resizer/resizer.sh
+++ offline/variants_tools/shell/resizer/resizer.sh
@@ -1,21 +1,34 @@
+#!/bin/bash
+
+echo "Resize picture with scale"
+
+if [ -z $1 ] ; then
+    echo "Missing argument 1 file name"
+    exit
+fi
+
+if [ ! -f $1 ] ; then
+    echo "File $1 does not exist"
+    exit
+fi
+
+if [ -z $2 ] ; then
+    echo "Missing argument 2 scale (in percent)"
+    exit
+fi
+
 w=$(convert $1 -format "%[w]" info:)
 h=$(convert $1 -format "%[h]" info:)
 echo "Before"
 echo "width=$w"
 echo "heigh=$h"
-cp $1 $1.orig
 
-# target  width 
-TARGET_WIDTH=100
+ratio=$2
 
+convert $1 -resize ${ratio}% $1.scaled
 
-ratio=$(( ($TARGET_WIDTH * 100) / $w ))
-echo $ratio
-
-convert $1.orig -resize ${ratio}% $1
-
-w=$(convert $1 -format "%[w]" info:)
-h=$(convert $1 -format "%[h]" info:)
+w=$(convert $1.scaled -format "%[w]" info:)
+h=$(convert $1.scaled -format "%[h]" info:)
 
 echo "After"
 echo "width=$w"
