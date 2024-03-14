dee9b127fa7d68ae4ffe0ca302566cb20c17b13b
lefranco <jeremie.lefrancois@gmail.com>
2024-03-14 19:23:32 +0100

    .

--- client-ihms-brython/tournament.py
+++ client-ihms-brython/tournament.py
@@ -861,7 +861,7 @@ def show_powers_results():
     MY_SUB_PANEL <= html.BR()
 
     information = html.DIV(Class='note')
-    information <= "Attention les parties sont considérées toutes comme terminées en l'état - à prendre en compte pour un tounoi en cours..."
+    information <= "Attention les parties sont considérées toutes comme terminées en l'état - à prendre en compte pour un tournoi en cours..."
     MY_SUB_PANEL <= information
     MY_SUB_PANEL <= html.BR()
 
