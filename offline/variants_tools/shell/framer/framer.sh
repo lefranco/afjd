b631184b0466ec579bb517de8b077c1720b07e81
lefranco <jeremie.lefrancois@gmail.com>
2024-03-27 19:10:59 +0100

    .

--- offline/variants_tools/shell/framer/framer.sh
+++ offline/variants_tools/shell/framer/framer.sh
@@ -14,13 +14,13 @@ fi
 
 w=$(convert $1 -format "%[w]" info:)
 h=$(convert $1 -format "%[h]" info:)
-cp $1 $1.framed
 
 # left
-convert $1.framed -strokewidth 1 -stroke black -draw "line 0,0 0,$(expr $h - 1)" $1.framed
+convert $1.framed -strokewidth 1 -stroke black -draw "line 0,0 0,$(expr $h - 1)" $1
 # right
-convert $1.framed -strokewidth 1 -stroke black -draw "line $(expr $w - 1),0 $(expr $w - 1),$(expr $h - 1)" $1.framed
+convert $1.framed -strokewidth 1 -stroke black -draw "line $(expr $w - 1),0 $(expr $w - 1),$(expr $h - 1)" $1
 # top
-convert $1.framed -strokewidth 1 -stroke black -draw "line 0,0 $(expr $w - 1), 0" $1.framed
+convert $1.framed -strokewidth 1 -stroke black -draw "line 0,0 $(expr $w - 1), 0" $1
 # down
-convert $1.framed -strokewidth 1 -stroke black -draw "line 0,$(expr $h - 1) $(expr $w - 1), $(expr $h - 1)" $1.framed
+convert $1.framed -strokewidth 1 -stroke black -draw "line 0,$(expr $h - 1) $(expr $w - 1), $(expr $h - 1)" $1
+
