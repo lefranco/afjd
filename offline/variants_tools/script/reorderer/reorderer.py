22d0d054067644956ae73df5db6d48bb54e5e1df
lefranco <jeremie.lefrancois@gmail.com>
2024-04-13 11:17:27 +0200

    .

--- offline/variants_tools/script/reorderer/reorderer.py
+++ offline/variants_tools/script/reorderer/reorderer.py
@@ -2,7 +2,11 @@
 
 
 """
-Reorder everythng to have a nice alphabetical order
+Reorder everythng after inserting a new regions...
+Bewate : still need to do :
+  - distancing
+  - neighbourhood (by hand)
+  - visibility
 """
 
 import argparse
@@ -12,7 +16,8 @@ import sys
 import json
 
 
-INSERT_POSITION = 5
+INSERT_POSITION = 5 # albania
+INSERT_TYPE = 1 # coast
 
 
 def reorder(json_variant_data: typing.Dict[str, typing.Any], json_parameters_data: typing.Dict[str, typing.Any]) -> None:
@@ -23,13 +28,13 @@ def reorder(json_variant_data: typing.Dict[str, typing.Any], json_parameters_dat
     # -------
 
     # regions
-    json_variant_data['regions'].insert(INSERT_POSITION - 1, -1)
+    json_variant_data['regions'].insert(INSERT_POSITION - 1, INSERT_TYPE)
 
     # centers
     json_variant_data['centers'] = list(map(lambda n: n + 1 if n >= INSERT_POSITION else n, json_variant_data['centers']))  # type: ignore
 
     # coastal_zones
-    json_variant_data['coastal_zones'] = list(map(lambda ll: [ll[0], ll[1] + 1] if ll[1] >= INSERT_POSITION else ll, json_variant_data['coastal_zones']))  # type: ignore
+    json_variant_data['coastal_zones'] = list(map(lambda t: [t[0] + 1, t[1]] if t[0] >= INSERT_POSITION else t, json_variant_data['coastal_zones']))  # type: ignore
 
     # start_units
     json_variant_data['start_units'] = list(map(lambda d: {k: list(map(lambda n: n + 1 if n >= INSERT_POSITION else n, v)) for k, v in d.items()}, json_variant_data['start_units']))  # type: ignore
