d607cd97c0a2b61d46d74262780b1438176c4a82
lefranco <jeremie.lefrancois@gmail.com>
2024-05-04 11:26:27 +0200

    .

--- client-ihms-brython/center_design.py
+++ client-ihms-brython/center_design.py
@@ -12,6 +12,7 @@ def stabbeur_center(x: int, y: int, ctx):  # pylint: disable=invalid-name
     """ display a center the stabbeur way """
 
     # the ctx.strokeStyle and ctx.fillStyle should be defined
+    ctx.lineWidth = 1
 
     ctx.beginPath()
     ctx.arc(x, y, CENTER_RAY, 0, 2 * pi, False)
