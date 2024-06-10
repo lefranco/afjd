0660d5a35bafd51510b2e7a835fbaac7285a9403
lefranco <jeremie.lefrancois@gmail.com>
2024-06-10 19:26:28 +0200

    .

--- client-ihms-brython/play_low.py
+++ client-ihms-brython/play_low.py
@@ -977,6 +977,37 @@ def game_incidents_reload(game_id):
     return incidents
 
 
+def game_incidents2_reload(game_id):
+    """ game_incidents2_reload """
+
+    incidents = []
+
+    def reply_callback(req):
+        nonlocal incidents
+        req_result = loads(req.text)
+        if req.status != 200:
+            if 'message' in req_result:
+                alert(f"Erreur à la récupération des incidents désordres civils  de la partie : {req_result['message']}")
+            elif 'msg' in req_result:
+                alert(f"Problème à la récupération des incidents désordres civils de la partie : {req_result['msg']}")
+            else:
+                alert("Réponse du serveur imprévue et non documentée")
+            return
+
+        incidents = req_result['incidents']
+
+    json_dict = {}
+
+    host = config.SERVER_CONFIG['GAME']['HOST']
+    port = config.SERVER_CONFIG['GAME']['PORT']
+    url = f"{host}:{port}/game-incidents2/{game_id}"
+
+    # extracting incidents from a game : no need for token
+    ajax.get(url, blocking=True, headers={'content-type': 'application/json'}, timeout=config.TIMEOUT_SERVER, data=dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
+
+    return incidents
+
+
 MY_PANEL = html.DIV()
 MY_PANEL.attrs['style'] = 'display: table-row'
 
