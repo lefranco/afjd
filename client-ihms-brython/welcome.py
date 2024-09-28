7e9dab4dfe5ccfbec406ff5e577bbd72a29754de
lefranco <jeremie.lefrancois@gmail.com>
2024-09-28 12:32:02 +0200

    welcome

--- client-ihms-brython/welcome.py
+++ client-ihms-brython/welcome.py
@@ -3,6 +3,11 @@
 # Message when successful creating account
 
 MESSAGE = """
-Maintenant vous devez vous identifier par le menu “Connexion“ !
+Après vous être connecté(e), vous pouvez tout de suite vous inscrire dans des parties en attente (boutons "Les parties" puis "Rejoindre une partie"). 
+Si vous hésitez entre différentes variantes, le bouton "Variantes" les décrit.
+En attendant que les parties démarrent, si vous ne connaissez pas bien les règles du jeu Diplomacy ou si vous voulez vous familiariser avec l'interface de Diplomania, nous vous recommandons de cliquer sur "Tutoriels et défis" et d'utiliser l' "Aide" ou le "Wiki".
+Vous pouvez aussi faire un tour sur le "Forum". 
+Et l'AFJD vous propose également son serveur Discord pour échanger. 
+Bienvenue et bonnes parties !
 """
 
