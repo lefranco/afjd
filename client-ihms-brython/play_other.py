25030c6963ac9510fa3c5ff29e1bc37b7a2683b4
lefranco <jeremie.lefrancois@gmail.com>
2024-11-16 20:50:47 +0100

    .

--- client-ihms-brython/play_other.py
+++ client-ihms-brython/play_other.py
@@ -72,7 +72,8 @@ def join_game():
             return
 
         messages = "<br>".join(req_result['msg'].split('\n'))
-        mydialog.InfoDialog("Information", f"Vous avez rejoint la partie (en utilisant un lien externe) : {messages}<br>C'est un engagement à ne pas prendre à la légère.<br>Pour son bon déroulement, le joueurs sont tenus de respecter les dates limites de rendu des ordres (DL). <br>En cas de souci, contacter l'arbitre par l'onglet 'Négociation'", True)
+        mydialog.InfoDialog("Information", f"Vous avez rejoint la partie (en utilisant un lien externe) : {messages}", True)
+        alert("Rejoindre une partie est un engagement à ne pas prendre à la légère !\nUn courriel vous préviendra de son démarrage mais revenez sur le site surveiller pour ne pas le manquer...")
 
     if play_low.PSEUDO is None:
         alert("Il faut se connecter au préalable")
@@ -939,7 +940,8 @@ def pairing():
                 return
 
             messages = "<br>".join(req_result['msg'].split('\n'))
-            mydialog.InfoDialog("Information", f"Vous avez rejoint la partie : {messages}<br>Attention, c'est un réel engagement à ne pas prendre à la légère.<br>Un abandon pourrait compromettre votre inscription à de futures parties sur le site...", True)
+            mydialog.InfoDialog("Information", f"Vous avez rejoint la partie : {messages})", True)
+            alert("Rejoindre une partie est un engagement à ne pas prendre à la légère !\nUn courriel vous préviendra de son démarrage mais revenez sur le site surveiller pour ne pas le manquer...")
 
             # back to where we started
             play_low.MY_SUB_PANEL.clear()
