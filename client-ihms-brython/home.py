47598d0c15d3b3e3abb7b1aec4c9acb8237f55f7
lefranco <jeremie.lefrancois@gmail.com>
2025-01-09 22:03:15 +0100

    .

--- client-ihms-brython/home.py
+++ client-ihms-brython/home.py
@@ -663,7 +663,7 @@ def show_news():
 
     # RGPD
     if 'RGPD_ACCEPTED' not in storage:
-        mydialog.InfoDialog("Règlement général sur la protection des données", "Vous êtes d'accord avec la manière dont le site utilise et conserve vos données personnelles. Si vous ne l'êtes pas, n'utilisez pas le site ! Plus de détail dans la page 'Données personnelles' accessible depuis le menu Accueil.", True)
+        mydialog.InfoDialog("Information", "Règlement général sur la protection des données :<br>Vous êtes d'accord avec la manière dont le site utilise et conserve vos données personnelles. Si vous ne l'êtes pas, n'utilisez pas le site ! Plus de détail dans la page 'Données personnelles' accessible depuis le menu Accueil.", True)
         storage['RGPD_ACCEPTED'] = 'yes'
 
 
