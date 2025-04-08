f9f472cf2fd8bc8d56d783508151b30e4470d9e7
lefranco <jeremie.lefrancois@gmail.com>
2025-04-08 20:28:20 +0200

    .

--- client-ihms-brython/allgames.py
+++ client-ihms-brython/allgames.py
@@ -185,7 +185,7 @@ def create_game(json_dict):
 
         if input_endgame_vote_allowed.checked:
             if not information_displayed_vote_allowed:
-                alert(f"Attention, le vote de fin de partie n'a de sens que pour les parties relativement longues, c'est à dire d'au moins 7 années.")
+                alert("Attention, le vote de fin de partie n'a de sens que pour les parties relativement longues, c'est à dire d'au moins 7 années.")
                 information_displayed_vote_allowed = True
 
     def display_just_play_callback(_):
