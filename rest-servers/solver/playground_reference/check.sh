ebfe61a74bba103bfe46b496e18ceb26b19a3516
lefranco <jeremie.lefrancois@gmail.com>
2025-04-10 18:56:10 +0200

    .

--- rest-servers/solver/playground_reference/check.sh
+++ rest-servers/solver/playground_reference/check.sh
@@ -6,21 +6,9 @@ export DIPLOCOM=$(pwd)/DIPLOCOM
 # pour executer les programmes
 export PATH=$PATH:$(pwd)/bin
 
-echo $PATH
 
-	lexique=descartes
-	carte=DESCARTES
+solveur -c DEFAULT -o ./ORDRES.ORD -i ./SITUATION.DAT -f RESULTAT.DAT
 
-pref=MOVES
-
-traducteur $pref.TXT2 $pref.ORD "${DIPLOCOM}/traduction/lexique_$lexique.txt" "${DIPLOCOM}/traduction/filtre.txt" 
-
-rm -f $pref.DAT
-
-aideur -c $carte -s -S P01  -o $pref.ORD -i $pref.DAT_ 
-
-sed 's/SAISON PRINTEMPS/SAISON AUTOMNE/' $pref.DAT_ > $pref.DAT
-
-
-solveur -c $carte -s -o $pref.ORD -i $pref.DAT -a $pref.TM1 -f $pref.TM3 
+echo "Resultat:"
+cat RESULTAT.DAT
 
