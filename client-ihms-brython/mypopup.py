0320c3d47f7f1625993ff825a3e8d4f6451aff01
lefranco <jeremie.lefrancois@gmail.com>
2025-05-01 18:21:37 +0200

    popup for having second information next to what doing last batch

--- client-ihms-brython/mypopup.py
+++ client-ihms-brython/mypopup.py
@@ -13,16 +13,25 @@ from browser.widgets.dialog import Dialog  # pylint: disable=import-error
 class Popup(Dialog):
     """ Slightly home made popup """
 
-    def __init__(self, title, canvas, content, button):
+    def __init__(self, title, canvas, content, button, resizeable):
+
+        # parent class
         Dialog.__init__(self, title)
+
+        # make it resizeable if required
+        if resizeable:
+            self.attrs['style'] += 'resize: both; overflow: auto;'
+
         # put image if there is one
         if canvas:
             self.panel <= canvas
             self.panel <= html.BR()
+
         # put content if there is some (text)
         if content:
             self.panel <= content
             self.panel <= html.BR()
+
         # put button if there is one (to access full information)
         if button:
             self.panel <= button
