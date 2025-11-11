f181114a19ddbd4f7f34b1fe8117e9cecec8308b
lefranco <jeremie.lefrancois@gmail.com>
2025-11-11 10:07:56 +0100

    .

--- offline/other_tools/dmarc/dmarc.py
+++ offline/other_tools/dmarc/dmarc.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
+
+"""DMARC."""
+
+from __future__ import annotations
+
 import imaplib
 import email
 from email.header import decode_header
-import yaml
 import argparse
-from pathlib import Path
+import pathlib
 import os
 import zipfile
 import xml.etree.ElementTree as ET
 import gzip
 import shutil
+import sys
+import typing
+import tkinter
+import tkinter.messagebox
+
+import yaml
 
 IMAP_SERVER = ''
 IMAP_PORT = 0
@@ -20,7 +30,7 @@ MAX_EMAILS = 0
 WORK_DIR = ''
 
 
-def read_config(config_file):
+def read_config(config_file: pathlib.Path) -> None:
     """ read_config """
 
     global IMAP_SERVER
@@ -28,13 +38,12 @@ def read_config(config_file):
     global IMAP_USER
     global IMAP_PASSWORD
     global IMAP_MAILBOX
-    global MAX_EMAILS
 
     global WORK_DIR
 
     if not config_file.is_file():
         print(f"Error : file '{config_file}' not found")
-        exit(1)
+        sys.exit(1)
 
     with open(config_file, "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
@@ -44,48 +53,108 @@ def read_config(config_file):
     IMAP_USER = config["imap"]["user"]
     IMAP_PASSWORD = config["imap"]["password"]
     IMAP_MAILBOX = config["imap"]["mailbox"]
-    MAX_EMAILS = int(config["imap"]["max_emails"])
 
     WORK_DIR = config["work_dir"]
 
-def parse_xml(xml_file):
 
-    print(f"{xml_file=}")
+ITEMS_DICT = {}
+IHM_TABLE = {}
+
+
+def parse_xml(xml_file: str) -> None:
+    """Parse an XML file."""
+
+    print(f"Parsing {xml_file=}...", end='')
 
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
+    for elem in root.iter():
+        if '}' in elem.tag:
+            elem.tag = elem.tag.split('}', 1)[1]  # garde seulement le nom après }
+
     # Récupérer les infos du rapport
     report_metadata = root.find("report_metadata")
+    assert report_metadata, "No metadata!"
     report_id = report_metadata.find("report_id").text
     org_name = report_metadata.find("org_name").text
-    print(f"Rapport {report_id} de {org_name}")
+    #  print(f"Rapport {report_id} de {org_name}")
 
     # Domaine testé
     policy = root.find("policy_published")
     domain = policy.find("domain").text
     adkim = policy.find("adkim").text
     aspf = policy.find("aspf").text
-    print(f"Domaine testé: {domain}, DKIM={adkim}, SPF={aspf}")
+    #  print(f"  Domaine testé: {domain}, DKIM={adkim}, SPF={aspf}")
 
     # Parcourir les enregistrements
+    attention = False   # TODO calculate this
+    stuff = []
     for record in root.findall("record"):
         source_ip = record.find("row/source_ip").text
         count = record.find("row/count").text
         disposition = record.find("row/policy_evaluated/disposition").text
         dkim = record.find("row/policy_evaluated/dkim").text
         spf = record.find("row/policy_evaluated/spf").text
-        print(f"IP: {source_ip}, Count: {count}, Disposition: {disposition}, DKIM: {dkim}, SPF: {spf}")    
+        line = f"  IP: {source_ip}, Count: {count}, Disposition: {disposition}, DKIM: {dkim}, SPF: {spf}"
+        stuff.append(line)
+
+    description = f"{org_name}/{report_id}"
+    
+    print(f"done!")
+    return description, attention, stuff
 
 
-def main():
+def display_callback(stuff: list[str]) -> None:
+    """Display information about content in email."""
+    tkinter.messagebox.showinfo("Info", '\n'.join(stuff))
+
+
+def delete_mail(uid_delete) -> None:
     """ main """
 
-    parser = argparse.ArgumentParser(description="IMAP parameters to read emails")
-    parser.add_argument("-c", "--config", required=True, help="Path to YAML file")
-    args = parser.parse_args()
-    config_file = Path(args.config)
-    read_config(config_file)
+    print(f"Connecting to {IMAP_SERVER}:{IMAP_PORT} ...")
+    imap = imaplib.IMAP4(IMAP_SERVER, IMAP_PORT)
+    imap.starttls()
+    imap.login(IMAP_USER, IMAP_PASSWORD)
+    imap.select(IMAP_MAILBOX)
+
+    typ, msg_data = imap.uid('SEARCH', None, 'UID', uid_delete) 
+    if typ != "OK":
+        print("Failed to search for emails")
+        return
+    if not msg_data[0]:
+        print(f"No email found with {uid_delete}")
+        return
+
+    # Mark the email for deletion
+    imap.uid('STORE', uid_delete, '+FLAGS', '\\Deleted')
+
+    # Permanently delete flagged emails
+    imap.expunge()
+    print("Deleted successfully.")
+
+    imap.logout()
+
+
+def delete_callback(uid: str, name: str) -> None:
+    """Delete email."""
+
+    # delete from server
+    delete_mail(uid)
+
+    # delete from ihm
+    display_button, delete_button = IHM_TABLE[name]
+    display_button.destroy()
+    delete_button.destroy()
+    del IHM_TABLE[name]
+
+
+TITLE = "My DMARC elements"
+
+
+def load_mails() -> None:
+    """ main """
 
     print(f"Connecting to {IMAP_SERVER}:{IMAP_PORT} ...")
     imap = imaplib.IMAP4(IMAP_SERVER, IMAP_PORT)
@@ -101,9 +170,9 @@ def main():
     print("Number of mails :", len(data[0].split()))
     print()
 
-    uids = data[0].split()[-MAX_EMAILS:]
+    uids = data[0].split()
     for uid in uids:
-        #print(f"UID {uid.decode()}")
+        #  print(f"UID {uid.decode()}")
         typ, msg_data = imap.fetch(uid, "(RFC822)")
         if typ != "OK":
             continue
@@ -113,13 +182,13 @@ def main():
         subject = decode_header(msg.get("Subject"))[0][0]
         if isinstance(subject, bytes):
             subject = subject.decode("utf-8", errors="replace")
-        #print("Subject: {subject}")
+        #  print("Subject: {subject}")
 
         # Sender
         from_ = decode_header(msg.get("From"))[0][0]
         if isinstance(from_, bytes):
             from_ = from_.decode("utf-8", errors="replace")
-        #print(f"From: {from_}")
+        #  print(f"From: {from_}")
 
         # Parcours des parties du mail
         for part in msg.walk():
@@ -134,52 +203,135 @@ def main():
                     if isinstance(filename, bytes):
                         filename = filename.decode("utf-8", errors="replace")
 
+                    # TODO code for zip and gz should be more similar
+
                     if filename.lower().endswith('zip'):
-                        filepath = os.path.join(WORK_DIR, filename)
-                        with open(filepath, "wb") as f:
-                            f.write(part.get_payload(decode=True))
-                        with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                        print("Found zip file!")
+                        zip_path = os.path.join(WORK_DIR, filename)
+                        with open(zip_path, "wb") as fic:
+                            fic.write(part.get_payload(decode=True))
+                        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                             files_in_zip = zip_ref.namelist()
                             zip_ref.extractall(WORK_DIR)
-                        os.remove(filepath)
+                        os.remove(zip_path)
                         extracted_files = [os.path.join(WORK_DIR, f) for f in files_in_zip]
                         for xml_file in extracted_files:
-                            assert xml_file.lower().endswith('xml'), "Not XML file"
-                            parse_xml(xml_file)
+                            assert xml_file.lower().endswith('xml'), f"{xml_file} : Not XML file"
+                            description, attention, stuff = parse_xml(xml_file)
+                            ITEMS_DICT[description] = (uid, attention, stuff)
 
                     elif filename.lower().endswith(".gz"):
-                        filepath = os.path.join(WORK_DIR, filename[:-3])
-                        with open(filepath, "wb") as f:
-                            f.write(part.get_payload(decode=True))
-                        with gzip.open(filepath, 'rb') as gzip_ref:
-                            extracted_file = os.path.join(WORK_DIR, os.path.basename(filepath)[:-3])
-                            files_in_gzip = [extracted_file]
-                            with open(out_path, 'wb') as f_out:
-                                shutil.copyfileobj(f_in, f_out)
-                        os.remove(filepath)         
-                        extracted_files = [os.path.join(WORK_DIR, f) for f in files_in_gzip]
-                        for xml_file in extracted_files:
-                            assert xml_file.lower().endswith('xml'), "Not XML file"
-                            parse_xml(xml_file)
+                        print("Found gz file!")
+                        gz_path = os.path.join(WORK_DIR, filename)
+                        with open(gz_path, "wb") as fic:
+                            fic.write(part.get_payload(decode=True))
+                        extracted_file = os.path.join(WORK_DIR, os.path.splitext(filename)[0])  # data.xml
+                        files_in_gzip = [extracted_file]
+                        with gzip.open(gz_path, 'rb') as f_in, open(extracted_file, 'wb') as f_out:
+                            shutil.copyfileobj(f_in, f_out)
+                        os.remove(gz_path)
+                        for xml_file in files_in_gzip:
+                            assert xml_file.lower().endswith('xml'), f"{xml_file} : Not XML file"
+                            description, attention, stuff = parse_xml(xml_file)
+                            ITEMS_DICT[description] = (uid, attention, stuff)
 
                     else:
                         pass
-                        # print("Unknown attachment type")               
+                        # print("Unknown attachment type")
 
-            # Body 
+            # Body
             elif content_type == "text/plain" and disposition is None:
-                body = part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8", errors="replace")
-                #print("Body (text) :")
-                #print(body)
+                pass
+                #  body = part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8", errors="replace")
+                #  print("Body (text) :")
+                #  print(body)
 
             elif content_type == "text/html" and disposition is None:
-                html_body = part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8", errors="replace")
-                #print("Body (HTML) :")
-                #print(html_body)
-
-        print("----------")
+                pass
+                #  html_body = part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8", errors="replace")
+                #  print("Body (HTML) :")
+                #  print(html_body)
 
     imap.logout()
+
+
+def position(root: typing.Any) -> None:
+    """Place the window on screen."""
+    root.update_idletasks()
+
+    # size of desktop
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # mouse position
+    try:
+        x_mouse = root.winfo_pointerx()
+        y_mouse = root.winfo_pointery()
+    except tkinter.TclError:
+        x_mouse = screen_width // 2
+        y_mouse = screen_height // 2
+
+    # size of window
+    window_width = root.winfo_width()
+    window_height = root.winfo_height()
+
+    # relative center
+    x = int(x_mouse - window_width / 2)
+    y = int(y_mouse - window_height / 2)
+
+    # make sure on screen
+    x = max(0, min(x, screen_width - window_width))
+    y = max(0, min(y, screen_height - window_height))
+
+    # geometry string
+    root.geometry(f"+{x}+{y}")
+
+
+def main() -> None:
+    """Main."""
+
+    parser = argparse.ArgumentParser(description="IMAP parameters to read emails")
+    parser.add_argument("-c", "--config", required=True, help="Path to YAML file")
+    args = parser.parse_args()
+    config_file = pathlib.Path(args.config)
+    read_config(config_file)
+
+    # from server
+    load_mails()
+
+    # create
+    root = tkinter.Tk()
+
+    # title
+    title_label = tkinter.Label(root, text=TITLE, font=("Arial", 10), fg="blue")
+    title_label.pack()
+
+    # frame for buttons
+    buttons_frame = tkinter.Frame(root)
+    buttons_frame.pack()
+
+    # all buttons inside
+    for i, (description, (uid, attention, stuff)) in enumerate(ITEMS_DICT.items()):
+
+        fg = 'Red' if attention else 'Black'
+
+        # to display
+        display_button = tkinter.Button(buttons_frame, text=description, font=("Arial", 8), fg=fg, command=lambda s=stuff: display_callback(s))
+        display_button.grid(row=i, column=0)
+
+        # to delete
+        delete_button = tkinter.Button(buttons_frame, text='delete me', font=("Arial", 8), fg=fg, command=lambda u=uid, d=description: delete_callback(u, d))
+        delete_button.grid(row=i, column=1)
+
+        # remember so to destroy
+        IHM_TABLE[description] = (display_button, delete_button)
+
+    # position window
+    position(root)
+
+    # tkinter loop
+    root.mainloop()
+
 
 if __name__ == "__main__":
     main()
