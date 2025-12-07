ca032566e1e407688b8d72e8b848571d584cd104
lefranco <jeremie.lefrancois@gmail.com>
2025-12-07 08:19:45 +0100

    .

--- ftp/forum_nodebb_old/mongo-init/mongo-init.js
+++
@@ -1,9 +0,0 @@
-// Initialisation simple
-db.createUser({
-  user: 'nodebb',
-  pwd: 'nodebbpassword123',
-  roles: [
-    { role: 'readWrite', db: 'nodebb' },
-    { role: 'dbAdmin', db: 'nodebb' }
-  ]
-});
