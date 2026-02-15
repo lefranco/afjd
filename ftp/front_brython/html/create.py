c6d82b328e5117c876f28ee9a58b40498723ffbc
lefranco <jeremie.lefrancois@gmail.com>
2026-02-15 17:37:30 +0100

    best perfs in tournaments

--- ftp/front_brython/html/create.py
+++ ftp/front_brython/html/create.py
@@ -33,37 +33,6 @@ MAX_NUMBER_GAMES = 200
 MAX_LEN_GAME_NAME = 50
 
 
-def get_tournament_players_data(tournament_id):
-    """ get_tournament_players_data : can return empty dict  (all games anonymous) """
-
-    tournament_players_dict = {}
-
-    def reply_callback(req):
-        nonlocal tournament_players_dict
-        req_result = loads(req.text)
-        if req.status != 200:
-            if 'message' in req_result:
-                alert(f"Erreur à la récupération de la liste des joueurs des parties du tournoi : {req_result['message']}")
-            elif 'msg' in req_result:
-                alert(f"Problème à la récupération de la liste des joueurs des partie du tournoi : {req_result['msg']}")
-            else:
-                alert("Réponse du serveur imprévue et non documentée")
-            return
-
-        tournament_players_dict = req_result
-
-    json_dict = {}
-
-    host = config.SERVER_CONFIG['GAME']['HOST']
-    port = config.SERVER_CONFIG['GAME']['PORT']
-    url = f"{host}:{port}/tournament-allocations/{tournament_id}"
-
-    # getting tournament allocation : need a token
-    ajax.get(url, blocking=True, headers={'content-type': 'application/json', 'AccessToken': storage['JWT_TOKEN']}, timeout=config.TIMEOUT_SERVER, data=dumps(json_dict), oncomplete=reply_callback, ontimeout=common.noreply_callback)
-
-    return tournament_players_dict
-
-
 def change_glorious():
     """ change_glorious """
 
@@ -705,7 +674,7 @@ def tournament_result():
 
     id2pseudo = {v: k for k, v in players_dict.items()}
 
-    tournament_players_dict = get_tournament_players_data(tournament_id)
+    tournament_players_dict = common.get_tournament_players_data(tournament_id)
 
     gamerole2pseudo = {(int(g), r): id2pseudo[int(p)] for g, d in tournament_players_dict.items() for p, r in d.items()}
 
