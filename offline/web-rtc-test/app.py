2cd74a5a688d0e71838c86ccce4a5c51a7113759
lefranco <jeremie.lefrancois@gmail.com>
2026-02-28 16:14:39 +0100

    .

--- offline/web-rtc-test/app.py
+++ offline/web-rtc-test/app.py
@@ -2,18 +2,23 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
+
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @socketio.on("join")
 def on_join(data):
     join_room(data["room"])
-
 
 @socketio.on("signal")
 def on_signal(data):
