d09144f91926934111b901865f96e30ce2f859eb
lefranco <jeremie.lefrancois@gmail.com>
2026-03-04 11:18:47 +0100

    .

--- ftp/api_flask/solver_service/playground_reference/check.sh
+++ ftp/api_flask/solver_service/playground_reference/check.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/bash
 
-# pour trouver les fichiers
+# to find files
 export DIPLOCOM=$(pwd)/DIPLOCOM
 
-# pour executer les programmes
+# to execute programs
 export PATH=$PATH:$(pwd)/bin
 
 
 solveur -c DEFAULT -o ./ORDRES.ORD -i ./SITUATION.DAT -f RESULTAT.DAT
 
-echo "Resultat:"
-cat RESULTAT.DAT
-rm RESULTAT.DAT
+if [ $? -eq 0 ] ; then
+    echo "Result:"
+    cat RESULTAT.DAT
+    rm RESULTAT.DAT
+fi
