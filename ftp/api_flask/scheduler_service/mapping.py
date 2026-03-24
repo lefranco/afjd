0580aa8e5f358f415a84b81c5f2ef4331c0fd19d
lefranco <jeremie.lefrancois@gmail.com>
2026-03-24 09:54:33 +0100

    .

--- ftp/api_flask/scheduler_service/mapping.py
+++ ftp/api_flask/scheduler_service/mapping.py
@@ -9,11 +9,12 @@ class RegionTypeEnum:
     COAST_REGION = 1
     LAND_REGION = 2
     SEA_REGION = 3
+    ISLAND_REGION = 4
 
     @staticmethod
     def inventory() -> typing.Tuple[int, ...]:
         """ inventory """
-        return (RegionTypeEnum.COAST_REGION, RegionTypeEnum.LAND_REGION, RegionTypeEnum.SEA_REGION)
+        return (RegionTypeEnum.COAST_REGION, RegionTypeEnum.LAND_REGION, RegionTypeEnum.SEA_REGION, RegionTypeEnum.ISLAND_REGION)
 
     @staticmethod
     def from_code(code: int) -> 'RegionTypeEnum':
@@ -102,10 +103,36 @@ class Center:
         # the region in which is the center
         self._region = region
 
+        # the owner at start of the game
+        self._owner_start: typing.Optional['Role'] = None
+
+        # free = no build
+        self._free = False
+
     @property
     def region(self) -> 'Region':
         """ property """
         return self._region
+
+    @property
+    def owner_start(self) -> typing.Optional['Role']:
+        """ property """
+        return self._owner_start
+
+    @owner_start.setter
+    def owner_start(self, owner_start: 'Role') -> None:
+        """ setter """
+        self._owner_start = owner_start
+
+    @property
+    def free(self) -> bool:
+        """ property """
+        return self._free
+
+    @free.setter
+    def free(self, free: bool) -> None:
+        """ setter """
+        self._free = free
 
     @property
     def identifier(self) -> int:
@@ -135,6 +162,9 @@ class Region:
 
         # the zone (for localisation)
         self._zone: typing.Optional[Zone] = None
+
+        # the unit occupying the region (for identifying units)
+        self._occupant = None
 
     @property
     def region_type(self) -> RegionTypeEnum:
@@ -181,6 +211,9 @@ class Zone:
         self._coast_type = coast_type
         self._parent_zone = parent_zone
 
+        # other zones one may access by fleet and army
+        self._neighbours: typing.Dict[int, typing.List[Zone]] = {u: [] for u in UnitTypeEnum.inventory()}
+
         # variant
         self._variant = variant
 
@@ -200,9 +233,29 @@ class Zone:
         return self._region
 
     @property
+    def neighbours(self) -> typing.Dict[int, typing.List['Zone']]:
+        """ property """
+        return self._neighbours
+
+    @neighbours.setter
+    def neighbours(self, neighbours: typing.Dict[int, typing.List['Zone']]) -> None:
+        """ setter """
+        self._neighbours = neighbours
+
+    @property
     def variant(self) -> 'Variant':
         """ property """
         return self._variant
+
+    @property
+    def parent_zone(self) -> typing.Optional['Zone']:
+        """ property """
+        return self._parent_zone
+
+    def __str__(self) -> str:
+        variant = self._variant
+        zone_full_name = variant.full_zone_name_table[self]
+        return f"La zone {zone_full_name}"
 
 
 class Role:
@@ -212,10 +265,40 @@ class Role:
 
         self._identifier = identifier
 
+        # start centers the role have
+        self._start_centers: typing.List[Center] = []
+
     @property
     def identifier(self) -> int:
         """ property """
         return self._identifier
+
+    @property
+    def start_centers(self) -> typing.List[Center]:
+        """ property """
+        return self._start_centers
+
+    @start_centers.setter
+    def start_centers(self, start_centers: typing.List[Center]) -> None:
+        """ setter """
+        self._start_centers = start_centers
+
+
+class EmergencyCenter:
+    """Emergency center for v1900 only"""
+    def __init__(self, role: Role, region: Region) -> None:
+        self._role = role
+        self._region = region
+
+    @property
+    def role(self) -> Role:
+        """ property """
+        return self._role
+
+    @property
+    def region(self) -> Region:
+        """ property """
+        return self._region
 
 
 ASCII_CONVERSION_TABLE = {
@@ -239,14 +322,22 @@ class Variant:
         # from variant file
         # =================
 
+        # build everywhere
+        self._build_everywhere = bool(raw_variant_content['build_everywhere'])
+
+        # start build
+        self._start_build = bool(raw_variant_content['start_build'])
+
         # extra_requirement_solo
-        self._extra_requirement_solo = bool(raw_variant_content['extra_requirement_solo'])
+        self._extra_requirement_solo = int(raw_variant_content['extra_requirement_solo'])
 
         # load the regions
         self._regions = {}
+        self._regions_types = set()
         for num, code in enumerate(raw_variant_content['regions']):
             number = num + 1
             region_type = RegionTypeEnum.from_code(code)
+            self._regions_types.add(region_type)
             assert region_type is not None
             region = Region(number, region_type)
             self._regions[number] = region
@@ -266,6 +357,32 @@ class Variant:
             number = num + 1
             role = Role(number)
             self._roles[number] = role
+
+        assert len(raw_variant_content['start_centers']) == len(self._roles)
+
+        # load start centers
+        for num, role_start_centers in enumerate(raw_variant_content['start_centers']):
+            number = num + 1
+            role = self._roles[number]
+            for number_center in role_start_centers:
+                start_center = self._centers[number_center]
+                start_center.owner_start = role
+                role.start_centers.append(start_center)
+
+        # load free centers
+        self._free_centers = []
+        for number_center in raw_variant_content['free_centers']:
+            free_center = self._centers[number_center]
+            free_center.free = True
+            self._free_centers.append(free_center)
+
+        # load emergency centers
+        self._emergency_centers = []
+        for num_role, num_region in raw_variant_content['emergency_centers']:
+            role = self._roles[num_role]
+            region = self._regions[num_region]
+            emergency_center = EmergencyCenter(role, region)
+            self._emergency_centers.append(emergency_center)
 
         # load the coast types
         self._coast_types = {}
@@ -298,15 +415,28 @@ class Variant:
             self._zones[offset + number] = zone
 
         # load the start units
+        self._start_units: typing.Dict[Role, typing.List[typing.Tuple[UnitTypeEnum, Zone]]] = {}
         for num, role_start_units in enumerate(raw_variant_content['start_units']):
             number = num + 1
             role = self._roles[number]
+            self._start_units[role] = []
             for unit_type_code_str, role_start_units2 in role_start_units.items():
                 unit_type_code = int(unit_type_code_str)
                 unit_type = UnitTypeEnum.from_code(unit_type_code)
                 assert unit_type is not None
                 for zone_num in role_start_units2:
                     zone = self._zones[zone_num]
+                    self._start_units[role].append((unit_type, zone))
+
+        # load the year zero and increment
+        self._year_zero = int(raw_variant_content['year_zero'])
+        self._increment = int(raw_variant_content['increment'])
+
+        # load the neighbouring
+        # not needed
+
+        # load the distancing
+        # not needed
 
         # =================
         # from parameters file
@@ -316,14 +446,16 @@ class Variant:
         self._unit_name_table: typing.Dict[UnitTypeEnum, str] = {}
         self._role_name_table: typing.Dict[Role, str] = {}
         self._coast_name_table: typing.Dict[CoastType, str] = {}
+        self._coast_full_name_table: typing.Dict[CoastType, str] = {}
         self._zone_name_table: typing.Dict[Zone, str] = {}
+        self._full_zone_name_table: typing.Dict[Zone, str] = {}
         self._season_name_table: typing.Dict[SeasonEnum, str] = {}
         self._order_name_table: typing.Dict[OrderTypeEnum, str] = {}
 
-        self._role_add_table = {}
+        self._role_add_table: typing.Dict[Role, typing.Tuple[str, str]] = {}
 
-        # load the regions type names
-        assert len(raw_parameters_content['regions']) == len(RegionTypeEnum.inventory())
+        # load the regions type names (islands may not be present)
+        assert len(raw_parameters_content['regions']) == len(self._regions_types)
         for region_type_code_str, data_dict in raw_parameters_content['regions'].items():
             region_type_code = int(region_type_code_str)
             region_type = RegionTypeEnum.from_code(region_type_code)
@@ -362,6 +494,7 @@ class Variant:
             coast_type_num = int(coast_type_num_str)
             coast_type = self._coast_types[coast_type_num]
             self._coast_name_table[coast_type] = data_dict['name']
+            self._coast_full_name_table[coast_type] = data_dict['full_name']
 
         # load the zones names and localisations (units and legends)
         assert len(raw_parameters_content['zones']) == len(self._zones)
@@ -371,14 +504,25 @@ class Variant:
 
             # special zones have a special name
             if zone.coast_type:
+
+                # name
                 assert zone.region.zone is not None
                 region_name = self._zone_name_table[zone.region.zone]
                 coast_name = self._coast_name_table[zone.coast_type]
                 name = f"{region_name}{coast_name}"
+
+                # full name
+                region_full_name = self._full_zone_name_table[zone.region.zone]
+                coast_full_name = self._coast_full_name_table[zone.coast_type]
+                full_name = f"{region_full_name} {coast_full_name}"
+
             else:
+                # much simpler !
                 name = data_dict['name']
+                full_name = data_dict['full_name']
 
             self._zone_name_table[zone] = name
+            self._full_zone_name_table[zone] = full_name
 
         # load seasons names
         assert len(raw_parameters_content['seasons']) == len(SeasonEnum.inventory())
@@ -427,9 +571,29 @@ class Variant:
 
         return {'roles': role_names, 'zones': zone_names, 'coasts': coast_names}
 
+    def role_adjective(self, role: Role) -> str:
+        """ role_adjective """
+        role_adj, _ = self._role_add_table[role]
+        return role_adj
+
     def number_centers(self) -> int:
         """ number_centers """
         return len(self._centers)
+
+    @property
+    def name(self) -> str:
+        """ property """
+        return self._name
+
+    @property
+    def region_name_table(self) -> typing.Dict[RegionTypeEnum, str]:
+        """ property """
+        return self._region_name_table
+
+    @property
+    def unit_name_table(self) -> typing.Dict[UnitTypeEnum, str]:
+        """ property """
+        return self._unit_name_table
 
     @property
     def role_name_table(self) -> typing.Dict[Role, str]:
@@ -437,11 +601,71 @@ class Variant:
         return self._role_name_table
 
     @property
+    def coast_name_table(self) -> typing.Dict[CoastType, str]:
+        """ property """
+        return self._coast_name_table
+
+    @property
+    def zone_name_table(self) -> typing.Dict[Zone, str]:
+        """ property """
+        return self._zone_name_table
+
+    @property
+    def full_zone_name_table(self) -> typing.Dict[Zone, str]:
+        """ property """
+        return self._full_zone_name_table
+
+    @property
+    def season_name_table(self) -> typing.Dict[SeasonEnum, str]:
+        """ property """
+        return self._season_name_table
+
+    @property
+    def order_name_table(self) -> typing.Dict[OrderTypeEnum, str]:
+        """ property """
+        return self._order_name_table
+
+    @property
     def roles(self) -> typing.Dict[int, Role]:
         """ property """
         return self._roles
 
     @property
-    def extra_requirement_solo(self) -> bool:
+    def start_units(self) -> typing.Dict[Role, typing.List[typing.Tuple[UnitTypeEnum, Zone]]]:
+        """ property """
+        return self._start_units
+
+    @property
+    def year_zero(self) -> int:
+        """ property """
+        return self._year_zero
+
+    @property
+    def increment(self) -> int:
+        """ property """
+        return self._increment
+
+    @property
+    def build_everywhere(self) -> bool:
+        """ property """
+        return self._build_everywhere
+
+    @property
+    def start_build(self) -> bool:
+        """ property """
+        return self._start_build
+
+    @property
+    def extra_requirement_solo(self) -> int:
         """ property """
         return self._extra_requirement_solo
+
+    @property
+    def free_centers(self) -> typing.List[Center]:
+        """ property """
+        return self._free_centers
+
+    @property
+    def emergency_centers(self) -> typing.List[EmergencyCenter]:
+        """ property """
+        return self._emergency_centers
